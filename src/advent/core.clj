7e4f35151ba7c2fed082090af349cc6e765f89b0
Eric Tobin <eatobin@gmail.com>
2019-12-06 12:22:42 -0600

    ->> all

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,12 +1,11 @@
 (ns advent.core
   (:require [clojure.string :as str]))
 
-(def modules-sum (->
+(def modules-sum (->>
                    "modules.txt"
                    (slurp)
                    (str/split-lines)
-                   (->>
-                     (map #(Integer/parseInt %))
-                     (into [])
-                     (map #(- (quot % 3) 2))
-                     (reduce +))))
+                   (map #(Integer/parseInt %))
+                   (into [])
+                   (map #(- (quot % 3) 2))
+                   (reduce +)))
