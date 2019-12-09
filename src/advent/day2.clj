623c264d4690daca8a58eabe4842cf49c9ff99c5
Eric Tobin <eatobin@gmail.com>
2019-12-09 16:54:35 -0600

    int-code works

--- src/advent/day2.clj
+++ src/advent/day2.clj
@@ -4,13 +4,11 @@
   (loop [offset 0
          ic ic]
     (let [ops (ic (+ 0 offset))]
-      (if (= ops 99)
-        ic
-        (let [a-pos (ic (+ 1 offset))
-              b-pos (ic (+ 2 offset))
-              place-pos (ic (+ 3 offset))]
-          (if (= ops 1)
-            (assoc ic place-pos (+ (ic a-pos) (ic b-pos)))
-            (assoc ic place-pos (* (ic a-pos) (ic b-pos))))
-          (recur (+ 4 offset)
-                 ic))))))
+      (case ops
+        99 ic
+        1 (recur
+            (+ 4 offset)
+            (assoc ic (ic (+ 3 offset)) (+ (ic (ic (+ 1 offset))) (ic (ic (+ 2 offset))))))
+        2 (recur
+            (+ 4 offset)
+            (assoc ic (ic (+ 3 offset)) (* (ic (ic (+ 1 offset))) (ic (ic (+ 2 offset))))))))))
