be74c4e75f4cff957c9275053c89d614a5c30a33
Eric Tobin <eatobin@gmail.com>
2019-12-08 22:35:51 -0600

    broken

--- src/advent/day2.clj
+++ src/advent/day2.clj
@@ -1,11 +1,16 @@
 (ns advent.day2)
 
-(defn mods [m]
+(defn int-code [ic]
   (loop [offset 0
-         m m
-         ops (m (+ 0 offset))]
-    (if (= ops 99)
-      m
-      (recur
-       (+ offset 4)
-       m))))
+         ic ic]
+    (let [ops (ic (+ 0 offset))]
+      (if (= ops 99)
+        ic
+        (let [a-pos (ic (+ 1 offset))
+              b-pos (ic (+ 2 offset))
+              place-pos (ic (+ 3 offset))]
+          (if (= ops 1)
+            (assoc ic place-pos (+ (ic a-pos) (ic b-pos)))
+            (assoc ic place-pos (* (ic a-pos) (ic b-pos))))
+          (recur (+ 4 offset)
+                 ic))))))
