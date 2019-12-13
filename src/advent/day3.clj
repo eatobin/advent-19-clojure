74c97c560fc62234773037b879929c05d5cc5eee
Eric Tobin <eatobin@gmail.com>
2019-12-13 15:46:08 -0600

    progress

--- src/advent/day3.clj
+++ src/advent/day3.clj
@@ -1,1 +1,20 @@
-(ns advent.day3)
+(ns advent.day3
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
+
+;part a
+(def both (with-open [reader (io/reader "paths.csv")]
+            (doall
+              (csv/read-csv reader))))
+(def red (->> (first both)
+              (into [])))
+
+(def blue (->> (second both)
+               (into [])))
+
+
+(defn direction [unit]
+  (subs unit 0 1))
+
+(defn distance [unit]
+  (Integer/parseInt (subs unit 1)))
