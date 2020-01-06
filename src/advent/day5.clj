b08e77b18a1df2a98ee2db465cae485961edf004
Eric Tobin <eatobin@gmail.com>
2020-01-05 21:33:46 -0600

    broken

--- src/advent/day5.clj
+++ src/advent/day5.clj
@@ -9,16 +9,44 @@
              (map #(Integer/parseInt %))
              (into [])))
 
-(def tester [3 7 1 7 6 6 1100 0])
+(def tester [3 7 1 7 6 6 1109 0])
 (def tester-2 [3 0 4 0 1101])
+
+;; (defn op-code [input memory]
+;;   (loop [pointer 0
+;;          memory memory
+;;          exit-code 999]
+;;     (let [instruction (memory (+ 0 pointer))]
+;;       (case instruction
+;;         1101 exit-code
+;;         1 (recur
+;;             (+ 4 pointer)
+;;             (assoc memory (memory (+ 3 pointer)) (+ (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer)))))
+;;             999)
+;;         2 (recur
+;;             (+ 4 pointer)
+;;             (assoc memory (memory (+ 3 pointer)) (* (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer)))))
+;;             999)
+;;         3 (recur
+;;             (+ 2 pointer)
+;;             (assoc memory (memory (inc pointer)) input)
+;;             999)
+;;         4 (recur
+;;             (+ 2 pointer)
+;;             memory
+;;             (memory (inc pointer)))))))
+
+(defn explode [num]
+  (vec (for [n (format "%05d" num)]
+         (- (byte n) 48))))
 
 (defn op-code [input memory]
   (loop [pointer 0
          memory memory
          exit-code 999]
-    (let [instruction (memory (+ 0 pointer))]
+    (let [instruction (int (last (explode (memory (+ 0 pointer)))))]
       (case instruction
-        1101 exit-code
+        9 exit-code
         1 (recur
             (+ 4 pointer)
             (assoc memory (memory (+ 3 pointer)) (+ (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer)))))
@@ -35,10 +63,6 @@
             (+ 2 pointer)
             memory
             (memory (inc pointer)))))))
-
-(defn explode [num]
-  (vec (for [n (format "%05d" num)]
-         (- (byte n) 48))))
 
 (defn op [number]
   (->
