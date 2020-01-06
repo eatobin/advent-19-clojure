c12a8a1afebb7ab5a4a1ab36e3db14b2e87e7307
Eric Tobin <eatobin@gmail.com>
2020-01-06 17:52:56 -0600

    better

--- src/advent/day5.clj
+++ src/advent/day5.clj
@@ -16,46 +16,46 @@
 (defn op-code [input memory]
   (loop [pointer 0
          memory memory
-         exit-code nil]
+         exit-code 0]
     (let [instruction (memory (+ 0 pointer))]
       (case instruction
         99 exit-code
         1 (recur
             (+ 4 pointer)
             (assoc memory (memory (+ 3 pointer)) (+ (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer)))))
-            0)
+            exit-code)
         101 (recur
               (+ 4 pointer)
               (assoc memory (memory (+ 3 pointer)) (+ (memory (+ 1 pointer)) (memory (memory (+ 2 pointer)))))
-              0)
+              exit-code)
         1001 (recur
                (+ 4 pointer)
                (assoc memory (memory (+ 3 pointer)) (+ (memory (memory (+ 1 pointer))) (memory (+ 2 pointer))))
-               0)
+               exit-code)
         1101 (recur
                (+ 4 pointer)
                (assoc memory (memory (+ 3 pointer)) (+ (memory (+ 1 pointer)) (memory (+ 2 pointer))))
-               0)
+               exit-code)
         2 (recur
             (+ 4 pointer)
             (assoc memory (memory (+ 3 pointer)) (* (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer)))))
-            0)
+            exit-code)
         102 (recur
               (+ 4 pointer)
               (assoc memory (memory (+ 3 pointer)) (* (memory (+ 1 pointer)) (memory (memory (+ 2 pointer)))))
-              0)
+              exit-code)
         1002 (recur
                (+ 4 pointer)
                (assoc memory (memory (+ 3 pointer)) (* (memory (memory (+ 1 pointer))) (memory (+ 2 pointer))))
-               0)
+               exit-code)
         1102 (recur
                (+ 4 pointer)
                (assoc memory (memory (+ 3 pointer)) (* (memory (+ 1 pointer)) (memory (+ 2 pointer))))
-               0)
+               exit-code)
         3 (recur
             (+ 2 pointer)
             (assoc memory (memory (+ 1 pointer)) input)
-            0)
+            exit-code)
         4 (recur
             (+ 2 pointer)
             memory
