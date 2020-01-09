e1fba911dcf1fcea66adcbddaa33eceb9e6fe8a1
Eric Tobin <eatobin@gmail.com>
2020-01-09 15:35:58 -0600

    fixed progress

--- src/advent/day5.clj
+++ src/advent/day5.clj
@@ -72,38 +72,38 @@
     (let [instruction (memory (+ 0 pointer))]
       (case instruction
         99 exit-code
-        ;1 (recur
-        ;    (+ 4 pointer)
-        ;    (assoc memory (memory (+ 3 pointer)) (+ (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer)))))
-        ;    exit-code)
-        ;101 (recur
-        ;      (+ 4 pointer)
-        ;      (assoc memory (memory (+ 3 pointer)) (+ (memory (+ 1 pointer)) (memory (memory (+ 2 pointer)))))
-        ;      exit-code)
-        ;1001 (recur
-        ;       (+ 4 pointer)
-        ;       (assoc memory (memory (+ 3 pointer)) (+ (memory (memory (+ 1 pointer))) (memory (+ 2 pointer))))
-        ;       exit-code)
-        ;1101 (recur
-        ;       (+ 4 pointer)
-        ;       (assoc memory (memory (+ 3 pointer)) (+ (memory (+ 1 pointer)) (memory (+ 2 pointer))))
-        ;       exit-code)
-        ;2 (recur
-        ;    (+ 4 pointer)
-        ;    (assoc memory (memory (+ 3 pointer)) (* (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer)))))
-        ;    exit-code)
-        ;102 (recur
-        ;      (+ 4 pointer)
-        ;      (assoc memory (memory (+ 3 pointer)) (* (memory (+ 1 pointer)) (memory (memory (+ 2 pointer)))))
-        ;      exit-code)
-        ;1002 (recur
-        ;       (+ 4 pointer)
-        ;       (assoc memory (memory (+ 3 pointer)) (* (memory (memory (+ 1 pointer))) (memory (+ 2 pointer))))
-        ;       exit-code)
-        ;1102 (recur
-        ;       (+ 4 pointer)
-        ;       (assoc memory (memory (+ 3 pointer)) (* (memory (+ 1 pointer)) (memory (+ 2 pointer))))
-        ;       exit-code)
+        1 (recur
+            (+ 4 pointer)
+            (assoc memory (memory (+ 3 pointer)) (+ (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer)))))
+            exit-code)
+        101 (recur
+              (+ 4 pointer)
+              (assoc memory (memory (+ 3 pointer)) (+ (memory (+ 1 pointer)) (memory (memory (+ 2 pointer)))))
+              exit-code)
+        1001 (recur
+               (+ 4 pointer)
+               (assoc memory (memory (+ 3 pointer)) (+ (memory (memory (+ 1 pointer))) (memory (+ 2 pointer))))
+               exit-code)
+        1101 (recur
+               (+ 4 pointer)
+               (assoc memory (memory (+ 3 pointer)) (+ (memory (+ 1 pointer)) (memory (+ 2 pointer))))
+               exit-code)
+        2 (recur
+            (+ 4 pointer)
+            (assoc memory (memory (+ 3 pointer)) (* (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer)))))
+            exit-code)
+        102 (recur
+              (+ 4 pointer)
+              (assoc memory (memory (+ 3 pointer)) (* (memory (+ 1 pointer)) (memory (memory (+ 2 pointer)))))
+              exit-code)
+        1002 (recur
+               (+ 4 pointer)
+               (assoc memory (memory (+ 3 pointer)) (* (memory (memory (+ 1 pointer))) (memory (+ 2 pointer))))
+               exit-code)
+        1102 (recur
+               (+ 4 pointer)
+               (assoc memory (memory (+ 3 pointer)) (* (memory (+ 1 pointer)) (memory (+ 2 pointer))))
+               exit-code)
         3 (recur
             (+ 2 pointer)
             (assoc memory (memory (+ 1 pointer)) input)
@@ -112,10 +112,10 @@
             (+ 2 pointer)
             memory
             (memory (memory (+ 1 pointer))))
-        ;104 (recur
-        ;      (+ 2 pointer)
-        ;      memory
-        ;      (memory (+ 1 pointer)))
+        104 (recur
+              (+ 2 pointer)
+              memory
+              (memory (+ 1 pointer)))
         8 (recur
             (+ 4 pointer)
             (if (= (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
