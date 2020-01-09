79ed45a4b83f52c98d5aed72fcc66afd4b04a6ed
Eric Tobin <eatobin@gmail.com>
2020-01-09 15:55:08 -0600

    fixed progress

--- src/advent/day5.clj
+++ src/advent/day5.clj
@@ -135,201 +135,201 @@
 
 ;part b
 
-;(defn op-code-2 [input memory]
-;  (loop [pointer 0
-;         memory memory
-;         exit-code 0]
-;    (let [instruction (memory pointer)]
-;      (case instruction
-;        99 exit-code
-;        1 (recur
-;            (+ 4 pointer)
-;            (assoc memory (memory (+ 3 pointer)) (+ (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer)))))
-;            exit-code)
-;        101 (recur
-;              (+ 4 pointer)
-;              (assoc memory (memory (+ 3 pointer)) (+ (memory (+ 1 pointer)) (memory (memory (+ 2 pointer)))))
-;              exit-code)
-;        1001 (recur
-;               (+ 4 pointer)
-;               (assoc memory (memory (+ 3 pointer)) (+ (memory (memory (+ 1 pointer))) (memory (+ 2 pointer))))
-;               exit-code)
-;        1101 (recur
-;               (+ 4 pointer)
-;               (assoc memory (memory (+ 3 pointer)) (+ (memory (+ 1 pointer)) (memory (+ 2 pointer))))
-;               exit-code)
-;        2 (recur
-;            (+ 4 pointer)
-;            (assoc memory (memory (+ 3 pointer)) (* (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer)))))
-;            exit-code)
-;        102 (recur
-;              (+ 4 pointer)
-;              (assoc memory (memory (+ 3 pointer)) (* (memory (+ 1 pointer)) (memory (memory (+ 2 pointer)))))
-;              exit-code)
-;        1002 (recur
-;               (+ 4 pointer)
-;               (assoc memory (memory (+ 3 pointer)) (* (memory (memory (+ 1 pointer))) (memory (+ 2 pointer))))
-;               exit-code)
-;        1102 (recur
-;               (+ 4 pointer)
-;               (assoc memory (memory (+ 3 pointer)) (* (memory (+ 1 pointer)) (memory (+ 2 pointer))))
-;               exit-code)
-;        3 (recur
-;            (+ 2 pointer)
-;            (assoc memory (memory (+ 1 pointer)) input)
-;            exit-code)
-;        4 (recur
-;            (+ 2 pointer)
-;            memory
-;            (memory (memory (+ 1 pointer))))
-;        104 (recur
-;              (+ 2 pointer)
-;              memory
-;              (memory (+ 1 pointer)))
-;        5 (recur
-;            (if (= 0 (memory (memory (+ 1 pointer))))
-;              (+ 3 pointer)
-;              (memory (memory (+ 2 pointer))))
-;            memory
-;            exit-code)
-;        105 (recur
-;              (if (= 0 (memory (+ 1 pointer)))
-;                (+ 3 pointer)
-;                (memory (memory (+ 2 pointer))))
-;              memory
-;              exit-code)
-;        1005 (recur
-;               (if (= 0 (memory (memory (+ 1 pointer))))
-;                 (+ 3 pointer)
-;                 (memory (+ 2 pointer)))
-;               memory
-;               exit-code)
-;        1105 (recur
-;               (if (= 0 (memory (+ 1 pointer)))
-;                 (+ 3 pointer)
-;                 (memory (+ 2 pointer)))
-;               memory
-;               exit-code)
-;        6 (recur
-;            (if (not= 0 (memory (memory (+ 1 pointer))))
-;              (+ 3 pointer)
-;              (memory (memory (+ 2 pointer))))
-;            memory
-;            exit-code)
-;        106 (recur
-;              (if (not= 0 (memory (+ 1 pointer)))
-;                (+ 3 pointer)
-;                (memory (memory (+ 2 pointer))))
-;              memory
-;              exit-code)
-;        1006 (recur
-;               (if (not= 0 (memory (memory (+ 1 pointer))))
-;                 (+ 3 pointer)
-;                 (memory (+ 2 pointer)))
-;               memory
-;               exit-code)
-;        1106 (recur
-;               (if (not= 0 (memory (+ 1 pointer)))
-;                 (+ 3 pointer)
-;                 (memory (+ 2 pointer)))
-;               memory
-;               exit-code)
-;        7 (recur
-;            (+ 4 pointer)
-;            (if (< (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
-;              (assoc memory (memory (memory (+ 3 pointer))) 1)
-;              (assoc memory (memory (memory (+ 3 pointer))) 0))
-;            exit-code)
-;        107 (recur
-;              (+ 4 pointer)
-;              (if (< (memory (+ 1 pointer)) (memory (memory (+ 2 pointer))))
-;                (assoc memory (memory (memory (+ 3 pointer))) 1)
-;                (assoc memory (memory (memory (+ 3 pointer))) 0))
-;              exit-code)
-;        1007 (recur
-;               (+ 4 pointer)
-;               (if (< (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))
-;                 (assoc memory (memory (memory (+ 3 pointer))) 1)
-;                 (assoc memory (memory (memory (+ 3 pointer))) 0))
-;               exit-code)
-;        1107 (recur
-;               (+ 4 pointer)
-;               (if (< (memory (+ 1 pointer)) (memory (+ 2 pointer)))
-;                 (assoc memory (memory (memory (+ 3 pointer))) 1)
-;                 (assoc memory (memory (memory (+ 3 pointer))) 0))
-;               exit-code)
-;        10007 (recur
-;                (+ 4 pointer)
-;                (if (< (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
-;                  (assoc memory (memory (+ 3 pointer)) 1)
-;                  (assoc memory (memory (+ 3 pointer)) 0))
-;                exit-code)
-;        10107 (recur
-;                (+ 4 pointer)
-;                (if (< (memory (+ 1 pointer)) (memory (memory (+ 2 pointer))))
-;                  (assoc memory (memory (+ 3 pointer)) 1)
-;                  (assoc memory (memory (+ 3 pointer)) 0))
-;                exit-code)
-;        11007 (recur
-;                (+ 4 pointer)
-;                (if (< (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))
-;                  (assoc memory (memory (+ 3 pointer)) 1)
-;                  (assoc memory (memory (+ 3 pointer)) 0))
-;                exit-code)
-;        11107 (recur
-;                (+ 4 pointer)
-;                (if (< (memory (+ 1 pointer)) (memory (+ 2 pointer)))
-;                  (assoc memory (memory (+ 3 pointer)) 1)
-;                  (assoc memory (memory (+ 3 pointer)) 0))
-;                exit-code)
-;        8 (recur
-;            (+ 4 pointer)
-;            (if (= (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
-;              (assoc memory (memory (+ 3 pointer)) 1)
-;              (assoc memory (memory (+ 3 pointer)) 0))
-;            exit-code)
-;        108 (recur
-;              (+ 4 pointer)
-;              (if (= (memory (+ 1 pointer)) (memory (memory (+ 2 pointer))))
-;                (assoc memory (memory (+ 3 pointer)) 1)
-;                (assoc memory (memory (+ 3 pointer)) 0))
-;              exit-code)
-;        1008 (recur
-;               (+ 4 pointer)
-;               (if (= (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))
-;                 (assoc memory (memory (+ 3 pointer)) 1)
-;                 (assoc memory (memory (+ 3 pointer)) 0))
-;               exit-code)
-;        1108 (recur
-;               (+ 4 pointer)
-;               (if (= (memory (+ 1 pointer)) (memory (+ 2 pointer)))
-;                 (assoc memory (memory (+ 3 pointer)) 1)
-;                 (assoc memory (memory (+ 3 pointer)) 0))
-;               exit-code)
-;        10008 (recur
-;                (+ 4 pointer)
-;                (if (= (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
-;                  (assoc memory (memory (+ 3 pointer)) 1)
-;                  (assoc memory (memory (+ 3 pointer)) 0))
-;                exit-code)
-;        10108 (recur
-;                (+ 4 pointer)
-;                (if (= (memory (+ 1 pointer)) (memory (memory (+ 2 pointer))))
-;                  (assoc memory (memory (+ 3 pointer)) 1)
-;                  (assoc memory (memory (+ 3 pointer)) 0))
-;                exit-code)
-;        11008 (recur
-;                (+ 4 pointer)
-;                (if (= (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))
-;                  (assoc memory (memory (+ 3 pointer)) 1)
-;                  (assoc memory (memory (+ 3 pointer)) 0))
-;                exit-code)
-;        11108 (recur
-;                (+ 4 pointer)
-;                (if (= (memory (+ 1 pointer)) (memory (+ 2 pointer)))
-;                  (assoc memory (memory (+ 3 pointer)) 1)
-;                  (assoc memory (memory (+ 3 pointer)) 0))
-;                exit-code)))))
+(defn op-code-2 [input memory]
+  (loop [pointer 0
+         memory memory
+         exit-code 0]
+    (let [instruction (memory pointer)]
+      (case instruction
+        99 exit-code
+        1 (recur
+            (+ 4 pointer)
+            (assoc memory (memory (+ 3 pointer)) (+ (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer)))))
+            exit-code)
+        101 (recur
+              (+ 4 pointer)
+              (assoc memory (memory (+ 3 pointer)) (+ (memory (+ 1 pointer)) (memory (memory (+ 2 pointer)))))
+              exit-code)
+        1001 (recur
+               (+ 4 pointer)
+               (assoc memory (memory (+ 3 pointer)) (+ (memory (memory (+ 1 pointer))) (memory (+ 2 pointer))))
+               exit-code)
+        1101 (recur
+               (+ 4 pointer)
+               (assoc memory (memory (+ 3 pointer)) (+ (memory (+ 1 pointer)) (memory (+ 2 pointer))))
+               exit-code)
+        2 (recur
+            (+ 4 pointer)
+            (assoc memory (memory (+ 3 pointer)) (* (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer)))))
+            exit-code)
+        102 (recur
+              (+ 4 pointer)
+              (assoc memory (memory (+ 3 pointer)) (* (memory (+ 1 pointer)) (memory (memory (+ 2 pointer)))))
+              exit-code)
+        1002 (recur
+               (+ 4 pointer)
+               (assoc memory (memory (+ 3 pointer)) (* (memory (memory (+ 1 pointer))) (memory (+ 2 pointer))))
+               exit-code)
+        1102 (recur
+               (+ 4 pointer)
+               (assoc memory (memory (+ 3 pointer)) (* (memory (+ 1 pointer)) (memory (+ 2 pointer))))
+               exit-code)
+        3 (recur
+            (+ 2 pointer)
+            (assoc memory (memory (+ 1 pointer)) input)
+            exit-code)
+        4 (recur
+            (+ 2 pointer)
+            memory
+            (memory (memory (+ 1 pointer))))
+        104 (recur
+              (+ 2 pointer)
+              memory
+              (memory (+ 1 pointer)))
+        5 (recur
+            (if (= 0 (memory (memory (+ 1 pointer))))
+              (+ 3 pointer)
+              (memory (memory (+ 2 pointer))))
+            memory
+            exit-code)
+        105 (recur
+              (if (= 0 (memory (+ 1 pointer)))
+                (+ 3 pointer)
+                (memory (memory (+ 2 pointer))))
+              memory
+              exit-code)
+        1005 (recur
+               (if (= 0 (memory (memory (+ 1 pointer))))
+                 (+ 3 pointer)
+                 (memory (+ 2 pointer)))
+               memory
+               exit-code)
+        1105 (recur
+               (if (= 0 (memory (+ 1 pointer)))
+                 (+ 3 pointer)
+                 (memory (+ 2 pointer)))
+               memory
+               exit-code)
+        6 (recur
+            (if (not= 0 (memory (memory (+ 1 pointer))))
+              (+ 3 pointer)
+              (memory (memory (+ 2 pointer))))
+            memory
+            exit-code)
+        106 (recur
+              (if (not= 0 (memory (+ 1 pointer)))
+                (+ 3 pointer)
+                (memory (memory (+ 2 pointer))))
+              memory
+              exit-code)
+        1006 (recur
+               (if (not= 0 (memory (memory (+ 1 pointer))))
+                 (+ 3 pointer)
+                 (memory (+ 2 pointer)))
+               memory
+               exit-code)
+        1106 (recur
+               (if (not= 0 (memory (+ 1 pointer)))
+                 (+ 3 pointer)
+                 (memory (+ 2 pointer)))
+               memory
+               exit-code)
+        7 (recur
+            (+ 4 pointer)
+            (if (< (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
+              (assoc memory (memory (+ 3 pointer)) 1)
+              (assoc memory (memory (+ 3 pointer)) 0))
+            exit-code)
+        107 (recur
+              (+ 4 pointer)
+              (if (< (memory (+ 1 pointer)) (memory (memory (+ 2 pointer))))
+                (assoc memory (memory (+ 3 pointer)) 1)
+                (assoc memory (memory (+ 3 pointer)) 0))
+              exit-code)
+        1007 (recur
+               (+ 4 pointer)
+               (if (< (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))
+                 (assoc memory (memory (+ 3 pointer)) 1)
+                 (assoc memory (memory (+ 3 pointer)) 0))
+               exit-code)
+        1107 (recur
+               (+ 4 pointer)
+               (if (< (memory (+ 1 pointer)) (memory (+ 2 pointer)))
+                 (assoc memory (memory (+ 3 pointer)) 1)
+                 (assoc memory (memory (+ 3 pointer)) 0))
+               exit-code)
+        10007 (recur
+                (+ 4 pointer)
+                (if (< (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
+                  (assoc memory (memory (+ 3 pointer)) 1)
+                  (assoc memory (memory (+ 3 pointer)) 0))
+                exit-code)
+        10107 (recur
+                (+ 4 pointer)
+                (if (< (memory (+ 1 pointer)) (memory (memory (+ 2 pointer))))
+                  (assoc memory (memory (+ 3 pointer)) 1)
+                  (assoc memory (memory (+ 3 pointer)) 0))
+                exit-code)
+        11007 (recur
+                (+ 4 pointer)
+                (if (< (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))
+                  (assoc memory (memory (+ 3 pointer)) 1)
+                  (assoc memory (memory (+ 3 pointer)) 0))
+                exit-code)
+        11107 (recur
+                (+ 4 pointer)
+                (if (< (memory (+ 1 pointer)) (memory (+ 2 pointer)))
+                  (assoc memory (memory (+ 3 pointer)) 1)
+                  (assoc memory (memory (+ 3 pointer)) 0))
+                exit-code)
+        8 (recur
+            (+ 4 pointer)
+            (if (= (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
+              (assoc memory (memory (+ 3 pointer)) 1)
+              (assoc memory (memory (+ 3 pointer)) 0))
+            exit-code)
+        108 (recur
+              (+ 4 pointer)
+              (if (= (memory (+ 1 pointer)) (memory (memory (+ 2 pointer))))
+                (assoc memory (memory (+ 3 pointer)) 1)
+                (assoc memory (memory (+ 3 pointer)) 0))
+              exit-code)
+        1008 (recur
+               (+ 4 pointer)
+               (if (= (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))
+                 (assoc memory (memory (+ 3 pointer)) 1)
+                 (assoc memory (memory (+ 3 pointer)) 0))
+               exit-code)
+        1108 (recur
+               (+ 4 pointer)
+               (if (= (memory (+ 1 pointer)) (memory (+ 2 pointer)))
+                 (assoc memory (memory (+ 3 pointer)) 1)
+                 (assoc memory (memory (+ 3 pointer)) 0))
+               exit-code)
+        10008 (recur
+                (+ 4 pointer)
+                (if (= (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
+                  (assoc memory (memory (+ 3 pointer)) 1)
+                  (assoc memory (memory (+ 3 pointer)) 0))
+                exit-code)
+        10108 (recur
+                (+ 4 pointer)
+                (if (= (memory (+ 1 pointer)) (memory (memory (+ 2 pointer))))
+                  (assoc memory (memory (+ 3 pointer)) 1)
+                  (assoc memory (memory (+ 3 pointer)) 0))
+                exit-code)
+        11008 (recur
+                (+ 4 pointer)
+                (if (= (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))
+                  (assoc memory (memory (+ 3 pointer)) 1)
+                  (assoc memory (memory (+ 3 pointer)) 0))
+                exit-code)
+        11108 (recur
+                (+ 4 pointer)
+                (if (= (memory (+ 1 pointer)) (memory (+ 2 pointer)))
+                  (assoc memory (memory (+ 3 pointer)) 1)
+                  (assoc memory (memory (+ 3 pointer)) 0))
+                exit-code)))))
 
 ;(def answer-2 (op-code-2 5 tv))
 ;(def jump-if-true [5 2 0 4 0 99])
