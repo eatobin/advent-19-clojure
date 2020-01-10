6bbe84a14e744b38450f67ff5104ecafa56e4714
Eric Tobin <eatobin@gmail.com>
2020-01-10 14:53:07 -0600

    add day6 progress

--- src/advent/day6.clj
+++ src/advent/day6.clj
@@ -1,3 +1,19 @@
 (ns advent.day6
-  (:require [clojure.data.csv :as csv]
-            [clojure.java.io :as io]))
+  (:require [clojure.string :as str]))
+
+(def tester "COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L")
+
+(def orbits (->>
+              tester
+              (str/split-lines)
+              (map #(str/split % #"\)"))))
