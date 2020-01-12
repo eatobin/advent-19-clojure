0e275d68cadbaa3b36b36da5e240b94cd7888586
Eric Tobin <eatobin@gmail.com>
2020-01-12 17:06:04 -0600

    broken

--- src/advent/day6.clj
+++ src/advent/day6.clj
@@ -32,10 +32,25 @@ A)B")
 ;;               (map #(str/split % #"\)"))
 ;;               (sort-by second)))
 
-(def parent-levels (sort-by first (group-by first (for [[p1 c1] orbits
-                                                        [p2 c2] orbits
-                                                        :when (= c1 p2)]
-                                                    [c1 c2]))))
+;(def parent-levels (vec (sort-by first (group-by first (for [[p1 c1] orbits
+;                                                             [p2 c2] orbits
+;                                                             :when (= c1 p2)]
+;                                                         [c1 c2])))))
+
+(def parent-levels (vec (sort-by first (group-by first (for [[p1 c1] orbits
+                                                             [p2 c2] orbits
+                                                             :when (= p1 p2)]
+                                                         [p1 c1])))))
+
+(def indexed-pl (map-indexed vector parent-levels))
 
 
 (def x (* (inc (first (first (map-indexed vector parent-levels)))) (count (second (second (first (map-indexed vector parent-levels)))))))
+
+(loop [orbits orbits
+       parents (conj [] (first (first orbits)))
+       children []]
+  (if (empty? orbits)
+    children
+    (recur (rest orbits)
+           )))
