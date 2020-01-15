bba2ecbe5a9bc89d710e28b8a3d7ea37cf365084
Eric Tobin <eatobin@gmail.com>
2020-01-15 15:29:19 -0600

    github progress

--- src/advent/day6.clj
+++ src/advent/day6.clj
@@ -1,58 +1,15 @@
 (ns advent.day6
   (:require [clojure.string :as str]))
 
-;(def tester "COM)B
-;B)C
-;C)D
-;D)E
-;E)F
-;B)G
-;G)H
-;D)I
-;E)J
-;J)K
-;K)L")
+; Parses the input format into a map of object -> orbit center
+(defn parse-orbit-map [raw]
+  (apply hash-map (flatten (map (comp reverse #(str/split % #"\)")) (str/split-lines raw)))))
 
-(def tester "COM)B
-COM)C
-B)D
-B)E
-C)F")
+(def input (->>
+             "resources/day6-tester-1.txt"
+             (slurp)
+             (parse-orbit-map)))
 
-(def orbits (->>
-              tester
-              (str/split-lines)
-              (map #(str/split % #"\)"))
-              (vec)))
-
-;; (def orbits (->>
-;;               "resources/day6.txt"
-;;               (slurp)
-;;               (str/split-lines)
-;;               (map #(str/split % #"\)"))
-;;               (sort-by second)))
-
-(defn parent?
-  [v coll]
-  (some #(= % v) coll))
-
-(loop [orbits [[:com :a] [:com :b] [:a :y] [:b :me] [:com :j] [:me :you]]
-       parents [:com]
-       kids []]
-  (if (empty? orbits)
-    parents
-    (recur
-      (rest orbits)
-      (if (parent? (first (first orbits)) parents)
-        (conj parents (second (first orbits)))
-        parents))))
-
-(loop [orbits []
-       generation 0
-       kids []
-       parents [:com]]
-  (if (empty? orbits)
-    [[generation parents]]
-    (let [new-kids (if (parent? (first (first orbits)) parents)
-                     (conj parents (second (first orbits)))
-                     parents)])))
+; Returns the number of orbits the given object is contained in (i.e. direct orbit + indirect orbits)
+(defn orbit-centers [orbit-map object]
+  (count (take-while some? (drop 1 (iterate orbit-map object)))))
