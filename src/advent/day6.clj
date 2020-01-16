807429db208c3ae817e2e6fb0b99b0a2f0ef701f
Eric Tobin <eatobin@gmail.com>
2020-01-16 13:18:38 -0600

    progress

--- src/advent/day6.clj
+++ src/advent/day6.clj
@@ -1,13 +1,14 @@
-;From:
-;https://github.com/jkoenig134/AdventOfCode-2019
-
-;part 1
-
 (ns advent.day6
   (:require [clojure.string :as str]))
 
-; Parses the input format into a map of object -> orbit center
-(defn parse-orbit-map [raw]
+;part 1
+
+;From:
+;https://github.com/jkoenig134/AdventOfCode-2019
+
+(defn parse-orbit-map
+  "Parses the input format into a map of object -> orbit center"
+  [raw]
   (apply hash-map (flatten (map (comp reverse #(str/split % #"\)")) (str/split-lines raw)))))
 
 (def input (->>
@@ -15,12 +16,14 @@
              (slurp)
              (parse-orbit-map)))
 
-; Returns the number of orbits the given object is contained in (i.e. direct orbit + indirect orbits)
-(defn orbit-centers [orbit-map object]
+(defn orbit-centers
+  "Returns the number of orbits the given object is contained in (i.e. direct orbit + indirect orbits)"
+  [orbit-map object]
   (count (take-while some? (drop 1 (iterate orbit-map object)))))
 
-; Returns the sum of the orbit centers for each object in the orbit map.
-(defn direct-and-indirect-orbits [orbit-map]
+(defn direct-and-indirect-orbits
+  "Returns the sum of the orbit centers for each object in the orbit map."
+  [orbit-map]
   (apply + (map (partial orbit-centers orbit-map) (keys orbit-map))))
 
 (println "Total number of (in)direct orbits:" (direct-and-indirect-orbits input))
@@ -33,3 +36,11 @@
                "resources/santa-advent.txt"
                (slurp)
                (parse-orbit-map)))
+
+(loop [you-path (take-while some? (drop 2 (iterate input-2 "YOU")))
+       santa-path (take-while some? (drop 1 (iterate input-2 "SAN")))]
+  (if (some #(= (first you-path) %) santa-path)
+    true
+    (recur
+      (rest you-path)
+      santa-path)))
