5d9a63d93ac99acee42b69031be519d5db52d495
Eric Tobin <eatobin@gmail.com>
2020-01-19 21:29:22 -0600

    example works

--- src/advent/day7.clj
+++ src/advent/day7.clj
@@ -12,9 +12,13 @@
 (def a [4, 3, 2, 1, 0])
 (def b [3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0])
 
-(def c [0,1,2,3,4])
-(def d [3,23,3,24,1002,24,10,24,1002,23,-1,23,
-        101,5,23,23,1,24,23,23,4,23,99,0,0])
+(def c [0, 1, 2, 3, 4])
+(def d [3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23,
+        101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0])
+
+(def e [1, 0, 4, 3, 2])
+(def f [3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33,
+        1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0])
 
 (defn op-code [phase input memory]
   (loop [pointer 0
@@ -214,6 +218,15 @@
                   (assoc memory (memory (+ 3 pointer)) 0))
                 exit-code)))))
 
-(def answer (op-code 0 0 tv))
+(def possibles (for [a (range 0 5)
+                     b (range 0 5)
+                     c (range 0 5)
+                     d (range 0 5)
+                     e (range 0 5)
+                     :when (distinct? a b c d e)]
+                 [a b c d e]))
+
+(defn pass [[a b c d e] i-code]
+  (op-code e (op-code d (op-code c (op-code b (op-code a 0 i-code) i-code) i-code) i-code) i-code))
 
 ;11981754
