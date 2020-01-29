5bdd289af65637e71f2c022cd871951d41ff8450
Eric Tobin <eatobin@gmail.com>
2020-01-29 14:24:30 -0600

    day8 progress!!

--- src/advent/day8.clj
+++ src/advent/day8.clj
@@ -1,3 +1,27 @@
 (ns advent.day8
-  (:require [clojure.data.csv :as csv]
-            [clojure.java.io :as io]))
+  (:require [clojure.core.matrix :as m]))
+
+(def data "123456789012")
+
+(defn to-ints [char-v]
+  (vec (for [c char-v]
+         (- (int c) 48))))
+
+(defn input-data [layer-size num-rows]
+  (->>
+    data
+    (partition layer-size)
+    (m/matrix)
+    (map to-ints)
+    (map #(partition num-rows %))
+    (m/matrix)))
+
+(defn input [layer-size num-rows]
+  (->>
+    "resources/day8.txt"
+    (slurp)
+    (partition layer-size)
+    (m/matrix)
+    (map to-ints)
+    (map #(partition num-rows %))
+    (m/matrix)))
