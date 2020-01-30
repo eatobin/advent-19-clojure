a48a087daa3030980fe4ae3bee81b5cb31920970
Eric Tobin <eatobin@gmail.com>
2020-01-30 17:16:48 -0600

    progress

--- src/advent/day8.clj
+++ src/advent/day8.clj
@@ -58,3 +58,36 @@
 (println answer)
 
 ;1485
+
+;part b
+
+(def sample (vec (take 5 (input 150 25))))
+(m/select-indices sample [[0 0]])
+;=> [[2 1 2 2 2 2 2 2 2 2 2 2 2 2 0 1 0 2 2 2 1 2 2 2 2]]
+(m/select-indices sample [[4 0]])
+;=> [[2 0 2 2 2 2 2 2 2 2 2 2 2 2 2 0 0 2 2 2 1 2 2 2 2]]
+(m/select-indices sample [[0 5]])
+;=> [[2 2 2 2 2 2 2 2 2 0 1 2 2 2 0 2 2 2 2 2 2 2 0 2 2]]
+(m/select-indices sample [[4 5]])
+;=> [[2 2 2 2 2 2 2 2 2 1 0 2 2 2 1 2 2 2 2 2 2 2 1 2 2]]
+(first (m/select-indices sample [[4 5]]))
+;=> [2 2 2 2 2 2 2 2 2 1 0 2 2 2 1 2 2 2 2 2 2 2 1 2 2]
+((first (m/select-indices sample [[4 5]])) 0)
+;=> 2
+((first (m/select-indices sample [[4 5]])) 1)
+;=> 2
+(count (first (m/select-indices sample [[4 5]])))
+;=> 25
+((first (m/select-indices sample [[0 0]])) 1)
+;=> 1
+((first (m/select-indices sample [[1 0]])) 1)
+;=> 2
+((first (m/select-indices sample [[2 0]])) 1)
+;=> 0
+((first (m/select-indices sample [[3 0]])) 1)
+;=> 2
+((first (m/select-indices sample [[4 0]])) 1)
+;=> 0
+
+(defn value-in-matrix [matrix [layer-number row-number column-number]]
+  ((first (m/select-indices matrix [[layer-number row-number]])) column-number))
