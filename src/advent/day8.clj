edcbe9a058fdb27d2a67a8b185b6f9721e36f5cc
Eric Tobin <eatobin@gmail.com>
2020-01-29 21:25:00 -0600

    part a works!!!

--- src/advent/day8.clj
+++ src/advent/day8.clj
@@ -1,20 +1,9 @@
 (ns advent.day8
   (:require [clojure.core.matrix :as m]))
 
-(def data "123456789012")
-
 (defn to-ints [char-v]
   (vec (for [c char-v]
          (- (int c) 48))))
-
-(defn input-data [layer-size num-rows]
-  (->>
-    data
-    (partition layer-size)
-    (m/matrix)
-    (map to-ints)
-    (map #(partition num-rows %))
-    (m/matrix)))
 
 (defn input [layer-size num-rows]
   (->>
@@ -39,7 +28,31 @@
   (vec (sort-by second < (map-indexed (fn [i l] (vector i (zeros-in-layer l)))
                                       layers))))
 
-(def smallest (first (first (zeros-in-layers (input 150 25)))))
+(defn smallest [layer-size num-rows] (first (first (zeros-in-layers (input layer-size num-rows)))))
 
 (defn small-layer [layer-size num-rows]
-  ((input layer-size num-rows) smallest))
+  ((input layer-size num-rows) (smallest layer-size num-rows)))
+
+(defn ones-in-row [row]
+  (->>
+    row
+    (filter #(= % 1))
+    (count)))
+
+(defn ones-in-layer [layer]
+  (reduce + (map ones-in-row layer)))
+
+(defn twos-in-row [row]
+  (->>
+    row
+    (filter #(= % 2))
+    (count)))
+
+(defn twos-in-layer [layer]
+  (reduce + (map twos-in-row layer)))
+
+(def answer (* (ones-in-layer (small-layer 150 25)) (twos-in-layer (small-layer 150 25))))
+
+(println answer)
+
+;1485
