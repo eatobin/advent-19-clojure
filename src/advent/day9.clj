72bf38b914344b75fde6a669bec419c9e8522729
Eric Tobin <eatobin@gmail.com>
2020-02-05 22:11:12 -0800

    progress

--- src/advent/day9.clj
+++ src/advent/day9.clj
@@ -4,252 +4,177 @@
 
 ;part a
 
-(def tv (->> (first (with-open [reader (io/reader "resources/day9.csv")]
+(def tv (->> (first (with-open [reader (io/reader "resources/day7.csv")]
                       (doall
                         (csv/read-csv reader))))
              (map #(Integer/parseInt %))
              (into [])))
 
-(defn op-code [phase input memory]
-  (loop [pointer 0
-         memory memory
-         exit-code 0
-         relative-base 0]
-    (let [instruction (memory pointer)]
-      (case instruction
-        99 exit-code
-        1 (recur
-            (+ 4 pointer)
-            (assoc memory (memory (+ 3 pointer)) (+ (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer)))))
-            exit-code
-            relative-base)
-        101 (recur
+(defn op-code-2 [[input phase pointer relative-base memory stopped?]]
+  (if stopped?
+    [input phase pointer relative-base memory true]
+    (loop [pointer pointer
+           relative-base relative-base
+           memory memory]
+      (let [instruction (memory pointer)]
+        (case instruction
+          99 [input phase pointer relative-base memory true]
+          1 (recur
               (+ 4 pointer)
-              (assoc memory (memory (+ 3 pointer)) (+ (memory (+ 1 pointer)) (memory (memory (+ 2 pointer)))))
-              exit-code
-              relative-base)
-        1001 (recur
-               (+ 4 pointer)
-               (assoc memory (memory (+ 3 pointer)) (+ (memory (memory (+ 1 pointer))) (memory (+ 2 pointer))))
-               exit-code
-               relative-base)
-        1101 (recur
-               (+ 4 pointer)
-               (assoc memory (memory (+ 3 pointer)) (+ (memory (+ 1 pointer)) (memory (+ 2 pointer))))
-               exit-code
-               relative-base)
-        2 (recur
-            (+ 4 pointer)
-            (assoc memory (memory (+ 3 pointer)) (* (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer)))))
-            exit-code
-            relative-base)
-        102 (recur
-              (+ 4 pointer)
-              (assoc memory (memory (+ 3 pointer)) (* (memory (+ 1 pointer)) (memory (memory (+ 2 pointer)))))
-              exit-code
-              relative-base)
-        1002 (recur
-               (+ 4 pointer)
-               (assoc memory (memory (+ 3 pointer)) (* (memory (memory (+ 1 pointer))) (memory (+ 2 pointer))))
-               exit-code
-               relative-base)
-        1102 (recur
-               (+ 4 pointer)
-               (assoc memory (memory (+ 3 pointer)) (* (memory (+ 1 pointer)) (memory (+ 2 pointer))))
-               exit-code
-               relative-base)
-        3 (recur
-            (+ 2 pointer)
-            (if (= 0 pointer)
-              (assoc memory (memory (+ 1 pointer)) phase)
-              (assoc memory (memory (+ 1 pointer)) input))
-            exit-code
-            relative-base)
-        4 (recur
-            (+ 2 pointer)
-            memory
-            (memory (memory (+ 1 pointer)))
-            relative-base)
-        104 (recur
-              (+ 2 pointer)
-              memory
-              (memory (+ 1 pointer))
-              relative-base)
-        5 (recur
-            (if (= 0 (memory (memory (+ 1 pointer))))
-              (+ 3 pointer)
-              (memory (memory (+ 2 pointer))))
-            memory
-            exit-code
-            relative-base)
-        105 (recur
-              (if (= 0 (memory (+ 1 pointer)))
-                (+ 3 pointer)
-                (memory (memory (+ 2 pointer))))
-              memory
-              exit-code
-              relative-base)
-        1005 (recur
-               (if (= 0 (memory (memory (+ 1 pointer))))
-                 (+ 3 pointer)
-                 (memory (+ 2 pointer)))
-               memory
-               exit-code
-               relative-base)
-        1105 (recur
-               (if (= 0 (memory (+ 1 pointer)))
-                 (+ 3 pointer)
-                 (memory (+ 2 pointer)))
-               memory
-               exit-code
-               relative-base)
-        6 (recur
-            (if (not= 0 (memory (memory (+ 1 pointer))))
-              (+ 3 pointer)
-              (memory (memory (+ 2 pointer))))
-            memory
-            exit-code
-            relative-base)
-        106 (recur
-              (if (not= 0 (memory (+ 1 pointer)))
-                (+ 3 pointer)
-                (memory (memory (+ 2 pointer))))
-              memory
-              exit-code
-              relative-base)
-        1006 (recur
-               (if (not= 0 (memory (memory (+ 1 pointer))))
-                 (+ 3 pointer)
-                 (memory (+ 2 pointer)))
-               memory
-               exit-code
-               relative-base)
-        1106 (recur
-               (if (not= 0 (memory (+ 1 pointer)))
-                 (+ 3 pointer)
-                 (memory (+ 2 pointer)))
-               memory
-               exit-code
-               relative-base)
-        7 (recur
-            (+ 4 pointer)
-            (if (< (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
-              (assoc memory (memory (+ 3 pointer)) 1)
-              (assoc memory (memory (+ 3 pointer)) 0))
-            exit-code
-            relative-base)
-        107 (recur
-              (+ 4 pointer)
-              (if (< (memory (+ 1 pointer)) (memory (memory (+ 2 pointer))))
-                (assoc memory (memory (+ 3 pointer)) 1)
-                (assoc memory (memory (+ 3 pointer)) 0))
-              exit-code
-              relative-base)
-        1007 (recur
-               (+ 4 pointer)
-               (if (< (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))
-                 (assoc memory (memory (+ 3 pointer)) 1)
-                 (assoc memory (memory (+ 3 pointer)) 0))
-               exit-code
-               relative-base)
-        1107 (recur
-               (+ 4 pointer)
-               (if (< (memory (+ 1 pointer)) (memory (+ 2 pointer)))
-                 (assoc memory (memory (+ 3 pointer)) 1)
-                 (assoc memory (memory (+ 3 pointer)) 0))
-               exit-code
-               relative-base)
-        10007 (recur
+              (assoc memory (memory (+ 3 pointer)) (+ (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))))
+          101 (recur
                 (+ 4 pointer)
-                (if (< (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
-                  (assoc memory (memory (+ 3 pointer)) 1)
-                  (assoc memory (memory (+ 3 pointer)) 0))
-                exit-code
-                relative-base)
-        10107 (recur
+                (assoc memory (memory (+ 3 pointer)) (+ (memory (+ 1 pointer)) (memory (memory (+ 2 pointer))))))
+          1001 (recur
+                 (+ 4 pointer)
+                 (assoc memory (memory (+ 3 pointer)) (+ (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))))
+          1101 (recur
+                 (+ 4 pointer)
+                 (assoc memory (memory (+ 3 pointer)) (+ (memory (+ 1 pointer)) (memory (+ 2 pointer)))))
+          2 (recur
+              (+ 4 pointer)
+              (assoc memory (memory (+ 3 pointer)) (* (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))))
+          102 (recur
+                (+ 4 pointer)
+                (assoc memory (memory (+ 3 pointer)) (* (memory (+ 1 pointer)) (memory (memory (+ 2 pointer))))))
+          1002 (recur
+                 (+ 4 pointer)
+                 (assoc memory (memory (+ 3 pointer)) (* (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))))
+          1102 (recur
+                 (+ 4 pointer)
+                 (assoc memory (memory (+ 3 pointer)) (* (memory (+ 1 pointer)) (memory (+ 2 pointer)))))
+          3 (recur
+              (+ 2 pointer)
+              (if (= 0 pointer)
+                (assoc memory (memory (+ 1 pointer)) phase)
+                (assoc memory (memory (+ 1 pointer)) input)))
+          4 [(memory (memory (+ 1 pointer))) phase (+ 2 pointer) memory false]
+          104 [(memory (+ 1 pointer)) phase (+ 2 pointer) memory false]
+          5 (recur
+              (if (= 0 (memory (memory (+ 1 pointer))))
+                (+ 3 pointer)
+                (memory (memory (+ 2 pointer))))
+              memory)
+          105 (recur
+                (if (= 0 (memory (+ 1 pointer)))
+                  (+ 3 pointer)
+                  (memory (memory (+ 2 pointer))))
+                memory)
+          1005 (recur
+                 (if (= 0 (memory (memory (+ 1 pointer))))
+                   (+ 3 pointer)
+                   (memory (+ 2 pointer)))
+                 memory)
+          1105 (recur
+                 (if (= 0 (memory (+ 1 pointer)))
+                   (+ 3 pointer)
+                   (memory (+ 2 pointer)))
+                 memory)
+          6 (recur
+              (if (not= 0 (memory (memory (+ 1 pointer))))
+                (+ 3 pointer)
+                (memory (memory (+ 2 pointer))))
+              memory)
+          106 (recur
+                (if (not= 0 (memory (+ 1 pointer)))
+                  (+ 3 pointer)
+                  (memory (memory (+ 2 pointer))))
+                memory)
+          1006 (recur
+                 (if (not= 0 (memory (memory (+ 1 pointer))))
+                   (+ 3 pointer)
+                   (memory (+ 2 pointer)))
+                 memory)
+          1106 (recur
+                 (if (not= 0 (memory (+ 1 pointer)))
+                   (+ 3 pointer)
+                   (memory (+ 2 pointer)))
+                 memory)
+          7 (recur
+              (+ 4 pointer)
+              (if (< (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
+                (assoc memory (memory (+ 3 pointer)) 1)
+                (assoc memory (memory (+ 3 pointer)) 0)))
+          107 (recur
                 (+ 4 pointer)
                 (if (< (memory (+ 1 pointer)) (memory (memory (+ 2 pointer))))
                   (assoc memory (memory (+ 3 pointer)) 1)
-                  (assoc memory (memory (+ 3 pointer)) 0))
-                exit-code
-                relative-base)
-        11007 (recur
-                (+ 4 pointer)
-                (if (< (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))
-                  (assoc memory (memory (+ 3 pointer)) 1)
-                  (assoc memory (memory (+ 3 pointer)) 0))
-                exit-code
-                relative-base)
-        11107 (recur
-                (+ 4 pointer)
-                (if (< (memory (+ 1 pointer)) (memory (+ 2 pointer)))
-                  (assoc memory (memory (+ 3 pointer)) 1)
-                  (assoc memory (memory (+ 3 pointer)) 0))
-                exit-code
-                relative-base)
-        8 (recur
-            (+ 4 pointer)
-            (if (= (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
-              (assoc memory (memory (+ 3 pointer)) 1)
-              (assoc memory (memory (+ 3 pointer)) 0))
-            exit-code
-            relative-base)
-        108 (recur
+                  (assoc memory (memory (+ 3 pointer)) 0)))
+          1007 (recur
+                 (+ 4 pointer)
+                 (if (< (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))
+                   (assoc memory (memory (+ 3 pointer)) 1)
+                   (assoc memory (memory (+ 3 pointer)) 0)))
+          1107 (recur
+                 (+ 4 pointer)
+                 (if (< (memory (+ 1 pointer)) (memory (+ 2 pointer)))
+                   (assoc memory (memory (+ 3 pointer)) 1)
+                   (assoc memory (memory (+ 3 pointer)) 0)))
+          10007 (recur
+                  (+ 4 pointer)
+                  (if (< (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
+                    (assoc memory (memory (+ 3 pointer)) 1)
+                    (assoc memory (memory (+ 3 pointer)) 0)))
+          10107 (recur
+                  (+ 4 pointer)
+                  (if (< (memory (+ 1 pointer)) (memory (memory (+ 2 pointer))))
+                    (assoc memory (memory (+ 3 pointer)) 1)
+                    (assoc memory (memory (+ 3 pointer)) 0)))
+          11007 (recur
+                  (+ 4 pointer)
+                  (if (< (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))
+                    (assoc memory (memory (+ 3 pointer)) 1)
+                    (assoc memory (memory (+ 3 pointer)) 0)))
+          11107 (recur
+                  (+ 4 pointer)
+                  (if (< (memory (+ 1 pointer)) (memory (+ 2 pointer)))
+                    (assoc memory (memory (+ 3 pointer)) 1)
+                    (assoc memory (memory (+ 3 pointer)) 0)))
+          8 (recur
               (+ 4 pointer)
-              (if (= (memory (+ 1 pointer)) (memory (memory (+ 2 pointer))))
+              (if (= (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
                 (assoc memory (memory (+ 3 pointer)) 1)
-                (assoc memory (memory (+ 3 pointer)) 0))
-              exit-code
-              relative-base)
-        1008 (recur
-               (+ 4 pointer)
-               (if (= (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))
-                 (assoc memory (memory (+ 3 pointer)) 1)
-                 (assoc memory (memory (+ 3 pointer)) 0))
-               exit-code
-               relative-base)
-        1108 (recur
-               (+ 4 pointer)
-               (if (= (memory (+ 1 pointer)) (memory (+ 2 pointer)))
-                 (assoc memory (memory (+ 3 pointer)) 1)
-                 (assoc memory (memory (+ 3 pointer)) 0))
-               exit-code
-               relative-base)
-        10008 (recur
-                (+ 4 pointer)
-                (if (= (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
-                  (assoc memory (memory (+ 3 pointer)) 1)
-                  (assoc memory (memory (+ 3 pointer)) 0))
-                exit-code
-                relative-base)
-        10108 (recur
+                (assoc memory (memory (+ 3 pointer)) 0)))
+          108 (recur
                 (+ 4 pointer)
                 (if (= (memory (+ 1 pointer)) (memory (memory (+ 2 pointer))))
                   (assoc memory (memory (+ 3 pointer)) 1)
-                  (assoc memory (memory (+ 3 pointer)) 0))
-                exit-code
-                relative-base)
-        11008 (recur
-                (+ 4 pointer)
-                (if (= (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))
-                  (assoc memory (memory (+ 3 pointer)) 1)
-                  (assoc memory (memory (+ 3 pointer)) 0))
-                exit-code
-                relative-base)
-        11108 (recur
-                (+ 4 pointer)
-                (if (= (memory (+ 1 pointer)) (memory (+ 2 pointer)))
-                  (assoc memory (memory (+ 3 pointer)) 1)
-                  (assoc memory (memory (+ 3 pointer)) 0))
-                exit-code
-                relative-base)
-        9 (recur
-            (+ 2 pointer)
-            memory
-            exit-code
-            (+ (memory (memory (+ 1 pointer))) relative-base))
-        109 (recur
+                  (assoc memory (memory (+ 3 pointer)) 0)))
+          1008 (recur
+                 (+ 4 pointer)
+                 (if (= (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))
+                   (assoc memory (memory (+ 3 pointer)) 1)
+                   (assoc memory (memory (+ 3 pointer)) 0)))
+          1108 (recur
+                 (+ 4 pointer)
+                 (if (= (memory (+ 1 pointer)) (memory (+ 2 pointer)))
+                   (assoc memory (memory (+ 3 pointer)) 1)
+                   (assoc memory (memory (+ 3 pointer)) 0)))
+          10008 (recur
+                  (+ 4 pointer)
+                  (if (= (memory (memory (+ 1 pointer))) (memory (memory (+ 2 pointer))))
+                    (assoc memory (memory (+ 3 pointer)) 1)
+                    (assoc memory (memory (+ 3 pointer)) 0)))
+          10108 (recur
+                  (+ 4 pointer)
+                  (if (= (memory (+ 1 pointer)) (memory (memory (+ 2 pointer))))
+                    (assoc memory (memory (+ 3 pointer)) 1)
+                    (assoc memory (memory (+ 3 pointer)) 0)))
+          11008 (recur
+                  (+ 4 pointer)
+                  (if (= (memory (memory (+ 1 pointer))) (memory (+ 2 pointer)))
+                    (assoc memory (memory (+ 3 pointer)) 1)
+                    (assoc memory (memory (+ 3 pointer)) 0)))
+          11108 (recur
+                  (+ 4 pointer)
+                  (if (= (memory (+ 1 pointer)) (memory (+ 2 pointer)))
+                    (assoc memory (memory (+ 3 pointer)) 1)
+                    (assoc memory (memory (+ 3 pointer)) 0)))
+          9 (recur
               (+ 2 pointer)
               memory
-              exit-code
-              (+ (memory (+ 1 pointer)) relative-base))))))
+              (+ (memory (memory (+ 1 pointer))) relative-base))
+          109 (recur
+                (+ 2 pointer)
+                memory
+                (+ (memory (+ 1 pointer)) relative-base)))))))
