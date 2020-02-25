f5fa9b9118d43ee29ae1dd3830bc2a639aa4bbdd
Eric Tobin <eatobin@gmail.com>
2020-02-24 20:51:01 -0800

    9 progress

--- src/advent/day9.clj
+++ src/advent/day9.clj
@@ -21,124 +21,114 @@
 (def sample-3 [104, 1125899906842624, 99])
 (def good-3 (into (sorted-map-by <) (zipmap (range) sample-3)))
 
-
-
-(defn param-mode-c [instruction pointer memory relative-base]
-  (case (instruction :c)
-    0 (get memory (memory (+ 1 pointer)) 0)
-    1 (memory (+ 1 pointer))
-    2 (get memory (+ (memory (+ 1 pointer)) relative-base) 0)))
-
-(defn param-mode-b [instruction pointer memory relative-base]
-  (case (instruction :b)
-    0 (get memory (memory (+ 2 pointer)) 0)
-    1 (memory (+ 2 pointer))
-    2 (get memory (+ (memory (+ 2 pointer)) relative-base) 0)))
-
-(defn param-mode-a [instruction pointer memory relative-base]
-  (case (instruction :a)
-    0 (memory (+ 3 pointer))
-    2 (get memory (+ (memory (+ 3 pointer)) relative-base) 0)))
-
-
-
-
-
-
-
 (defn pad-5 [n]
   (zipmap [:a :b :c :d :e]
           (for [n (format "%05d" n)]
             (- (byte n) 48))))
 
-(defn param-c-ir-pw-iw [pointer memory]
-  (memory (+ 1 pointer)))
+; y1
+(defn a-p-w [pointer memory]
+  (memory (+ 3 pointer)))
 
-(defn param-c-pr-rr [pointer memory relative-base]
-  (get memory (+ (memory (+ 1 pointer)) relative-base) 0))
-
-(defn param-c-rw [pointer memory relative-base]
-  (+ (memory (+ 1 pointer)) relative-base))
-
-(defn param-b-pr-rr [pointer memory relative-base]
+; y2, y9
+(defn b-p-r-b-r-r [pointer memory relative-base]
   (get memory (+ (memory (+ 2 pointer)) relative-base) 0))
 
-(defn param-b-ir [pointer memory]
+; y3, y10
+(defn c-p-r-c-r-r [pointer memory relative-base]
+  (get memory (+ (memory (+ 1 pointer)) relative-base) 0))
+
+; y4, y6
+(defn c-p-w-c-i-r [pointer memory]
+  (memory (+ 1 pointer)))
+
+; y5
+(defn b-i-r [pointer memory]
   (memory (+ 2 pointer)))
 
-(defn param-a-pw-iw [pointer memory]
-  (memory (+ 3 pointer)))
+; y7
+(defn a-r-w [pointer memory relative-base]
+  (+ (memory (+ 3 pointer)) relative-base))
+
+; y8
+(defn c-r-w [pointer memory relative-base]
+  (+ (memory (+ 1 pointer)) relative-base))
 
 (defn param-maker-c [instruction pointer memory relative-base]
   (case (instruction :e)
     1 (case (instruction :c)
-        0 (param-c-pr-rr pointer memory 0)
-        1 (param-c-ir-pw-iw pointer memory)
-        2 (param-c-pr-rr pointer memory relative-base))
+        0 (c-p-r-c-r-r pointer memory 0)
+        1 (c-p-w-c-i-r pointer memory)
+        2 (c-p-r-c-r-r pointer memory relative-base))
     2 (case (instruction :c)
-        0 (param-c-pr-rr pointer memory 0)
-        1 (param-c-ir-pw-iw pointer memory)
-        2 (param-c-pr-rr pointer memory relative-base))
+        0 (c-p-r-c-r-r pointer memory 0)
+        1 (c-p-w-c-i-r pointer memory)
+        2 (c-p-r-c-r-r pointer memory relative-base))
     3 (case (instruction :c)
-        0 (param-c-ir-pw-iw pointer memory)
-        1 (param-c-ir-pw-iw pointer memory)
-        2 (param-c-rw pointer memory relative-base))
+        0 (c-p-w-c-i-r pointer memory)
+        2 (c-r-w pointer memory relative-base))
     4 (case (instruction :c)
-        0 (param-c-pr-rr pointer memory 0)
-        1 (param-c-pr-rr pointer memory 0)
-        2 (param-c-pr-rr pointer memory relative-base))
+        0 (c-p-r-c-r-r pointer memory 0)
+        1 (c-p-w-c-i-r pointer memory)
+        2 (c-p-r-c-r-r pointer memory relative-base))
     5 (case (instruction :c)
-        0 (param-c-pr-rr pointer memory 0)
-        1 (param-c-ir-pw-iw pointer memory)
-        2 (param-c-pr-rr pointer memory relative-base))
+        0 (c-p-r-c-r-r pointer memory 0)
+        1 (c-p-w-c-i-r pointer memory)
+        2 (c-p-r-c-r-r pointer memory relative-base))
     6 (case (instruction :c)
-        0 (param-c-pr-rr pointer memory 0)
-        1 (param-c-ir-pw-iw pointer memory)
-        2 (param-c-pr-rr pointer memory relative-base))
+        0 (c-p-r-c-r-r pointer memory 0)
+        1 (c-p-w-c-i-r pointer memory)
+        2 (c-p-r-c-r-r pointer memory relative-base))
     7 (case (instruction :c)
-        0 (param-c-pr-rr pointer memory 0)
-        1 (param-c-ir-pw-iw pointer memory)
-        2 (param-c-pr-rr pointer memory relative-base))
+        0 (c-p-r-c-r-r pointer memory 0)
+        1 (c-p-w-c-i-r pointer memory)
+        2 (c-p-r-c-r-r pointer memory relative-base))
     8 (case (instruction :c)
-        0 (param-c-pr-rr pointer memory 0)
-        1 (param-c-ir-pw-iw pointer memory)
-        2 (param-c-pr-rr pointer memory relative-base))))
+        0 (c-p-r-c-r-r pointer memory 0)
+        1 (c-p-w-c-i-r pointer memory)
+        2 (c-p-r-c-r-r pointer memory relative-base))))
 
 (defn param-maker-b [instruction pointer memory]
   (case (instruction :e)
     1 (case (instruction :b)
-        0 (param-b-pr-rr pointer memory 0)
-        1 (param-b-ir pointer memory))
+        0 (b-p-r-b-r-r pointer memory 0)
+        1 (b-i-r pointer memory)
+        2 (b-p-r-b-r-r pointer memory 0))
     2 (case (instruction :b)
-        0 (param-b-pr-rr pointer memory 0)
-        1 (param-b-ir pointer memory))
+        0 (b-p-r-b-r-r pointer memory 0)
+        1 (b-i-r pointer memory)
+        2 (b-p-r-b-r-r pointer memory 0))
     5 (case (instruction :b)
-        0 (param-b-pr-rr pointer memory 0)
-        1 (param-b-ir pointer memory))
+        0 (b-p-r-b-r-r pointer memory 0)
+        1 (b-i-r pointer memory)
+        2 (b-p-r-b-r-r pointer memory 0))
     6 (case (instruction :b)
-        0 (param-b-pr-rr pointer memory 0)
-        1 (param-b-ir pointer memory))
+        0 (b-p-r-b-r-r pointer memory 0)
+        1 (b-i-r pointer memory)
+        2 (b-p-r-b-r-r pointer memory 0))
     7 (case (instruction :b)
-        0 (param-b-pr-rr pointer memory 0)
-        1 (param-b-ir pointer memory))
+        0 (b-p-r-b-r-r pointer memory 0)
+        1 (b-i-r pointer memory)
+        2 (b-p-r-b-r-r pointer memory 0))
     8 (case (instruction :b)
-        0 (param-b-pr-rr pointer memory 0)
-        1 (param-b-ir pointer memory))))
+        0 (b-p-r-b-r-r pointer memory 0)
+        1 (b-i-r pointer memory)
+        2 (b-p-r-b-r-r pointer memory 0))))
 
-(defn param-maker-a [instruction pointer memory]
+(defn param-maker-a [instruction pointer memory relative-base]
   (case (instruction :e)
     1 (case (instruction :a)
-        0 (param-a-pw-iw pointer memory)
-        1 (param-a-pw-iw pointer memory))
+        0 (a-p-w pointer memory)
+        2 (a-r-w pointer memory relative-base))
     2 (case (instruction :a)
-        0 (param-a-pw-iw pointer memory)
-        1 (param-a-pw-iw pointer memory))
+        0 (a-p-w pointer memory)
+        2 (a-r-w pointer memory relative-base))
     7 (case (instruction :a)
-        0 (param-a-pw-iw pointer memory)
-        1 (param-a-pw-iw pointer memory))
+        0 (a-p-w pointer memory)
+        2 (a-r-w pointer memory relative-base))
     8 (case (instruction :a)
-        0 (param-a-pw-iw pointer memory)
-        1 (param-a-pw-iw pointer memory))))
+        0 (a-p-w pointer memory)
+        2 (a-r-w pointer memory relative-base))))
 
 (defn op-code [[input phase pointer relative-base memory stopped? recur?]]
   (if stopped?
