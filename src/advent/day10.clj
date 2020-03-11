00d54e4c40f09ba10d3eb94a10677d935c2726a1
Eric Tobin <eatobin@gmail.com>
2020-03-11 18:03:07 -0500

    works!!!

--- src/advent/day10.clj
+++ src/advent/day10.clj
@@ -50,36 +50,21 @@
 ;[[17 22] 288]
 
 ;part b
-(defn degree [[x0 y0] [x1 y1]]
-  (cond
-    (and (= y0 y1) (< x0 x1)) 0.0
-    (and (= y0 y1) (> x0 x1)) 180.0
-    (and (= x0 x1) (< y0 y1)) 90.0
-    (and (= x0 x1) (> y0 y1)) 270.0
-    ;x inc, y inc - A
-    (and (< x0 x1) (< y0 y1)) (/ (* (trig/atan (/ (- y1 y0) (- x1 x0))) 180) Math/PI)
-    ;x dec, y dec - C
-    (and (> x0 x1) (> y0 y1)) (+ (/ (* (trig/atan (/ (- y1 y0) (- x1 x0))) 180) Math/PI) 180)
-    ;x inc, y dec - D
-    (and (< x0 x1) (> y0 y1)) (+ (/ (* (trig/atan (/ (- y1 y0) (- x1 x0))) 180) Math/PI) 360)
-    ;x dec, y inc - B
-    (and (> x0 x1) (< y0 y1)) (+ (/ (* (trig/atan (/ (- y1 y0) (- x1 x0))) 180) Math/PI) 180)))
-
-(degree [0 0] [1 0])
+(slope [0 0] [1 0])
 ;=> 0.0
-(degree [0 0] [1 1])
+(slope [0 0] [1 1])
 ;=> 45.0
-(degree [0 0] [0 1])
+(slope [0 0] [0 1])
 ;=> 90.0
-(degree [0 0] [-1 1])
+(slope [0 0] [-1 1])
 ;=> 135.0
-(degree [0 0] [-1 0])
+(slope [0 0] [-1 0])
 ;=> 180.0
-(degree [0 0] [-1 -1])
+(slope [0 0] [-1 -1])
 ;=> 225.0
-(degree [0 0] [0 -1])
+(slope [0 0] [0 -1])
 ;=> 270.0
-(degree [0 0] [30000 -1])
+(slope [0 0] [30000 -1])
 ;=> 359.9980901406836
 
 (vec (distinct (slopes [17 22] asteroid-points-vec)))
