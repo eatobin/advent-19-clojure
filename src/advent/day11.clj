1536dfb555230263e657954c2e970400b75a5aaa
Eric Tobin <eatobin@gmail.com>
2020-04-02 20:29:29 -0500

    new-heading

--- src/advent/day11.clj
+++ src/advent/day11.clj
@@ -3,3 +3,12 @@
 
 ;part a
 (def tv (ic/make-tv "resources/day11.csv"))
+
+(defn new-heading [heading turn]
+  (cond
+    (and (= heading 0) (= turn 0)) 3
+    (and (= heading 3) (= turn 0)) 2
+    (and (= heading 2) (= turn 0)) 1
+    (and (= heading 1) (= turn 0)) 0
+    (and (= heading 3) (= turn 1)) 0
+    :else (+ heading turn)))
