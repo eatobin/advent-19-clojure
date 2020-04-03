9899acb6df59d1e6434ed0277d47a510de4718a5
Eric Tobin <eatobin@gmail.com>
2020-04-02 21:17:23 -0500

    new-point

--- src/advent/day11.clj
+++ src/advent/day11.clj
@@ -12,3 +12,12 @@
     (and (= heading 1) (= turn 0)) 0
     (and (= heading 3) (= turn 1)) 0
     :else (+ heading turn)))
+
+(defn new-point [{:keys [x y]} h]
+  (case h
+    0 {:x x :y (+ 1 y)}
+    1 {:x (+ 1 x) :y y}
+    2 {:x x :y (+ y -1)}
+    3 {:x (+ x -1) :y y}))
+
+{:pt {:x 0 :y 0} :h 0 :c :b :p :w :t 0}
