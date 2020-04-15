c08833300f6ab3ac24324e9e397324da8be0b1c6
Eric Tobin <eatobin@gmail.com>
2020-04-14 20:05:23 -0500

    new directions

--- src/advent/day11.clj
+++ src/advent/day11.clj
@@ -8,12 +8,14 @@
 
 (defn new-heading [heading turn]
   (cond
-    (and (= heading 0) (= turn 0)) 3
-    (and (= heading 3) (= turn 0)) 2
-    (and (= heading 2) (= turn 0)) 1
-    (and (= heading 1) (= turn 0)) 0
-    (and (= heading 3) (= turn 1)) 0
-    :else (+ heading turn)))
+    (and (= heading :n) (= turn 0)) :w
+    (and (= heading :n) (= turn 1)) :e
+    (and (= heading :e) (= turn 0)) :n
+    (and (= heading :e) (= turn 1)) :s
+    (and (= heading :s) (= turn 0)) :e
+    (and (= heading :s) (= turn 1)) :w
+    (and (= heading :w) (= turn 0)) :s
+    (and (= heading :w) (= turn 1)) :n))
 
 (defn new-point [{:keys [x y]} h]
   (case h
@@ -24,42 +26,42 @@
 
 (def state {:pt {:x 0 :y 0} :c 0})
 
-(def states (atom [{:pt {:x 0, :y 0}, :h 0, :c 0}]))
+(def states (atom [{:pt {:x 0, :y 0}, :h :n, :c 0}]))
 
 (def many-states [{:pt {:x 0, :y 0}, :c 1}
                   {:pt {:x 1, :y 1}, :c 1}
                   {:pt {:x 2, :y 2}, :c 0}
-                  {:pt {:x 3, :y 3}, :h 2, :c 0}])
+                  {:pt {:x 3, :y 3}, :h :s, :c 0}])
 
-(defn map-eq-pts [tpt pts]
-  (vec (map (fn [{pt :pt c :c}] (if (= tpt pt) c nil)) pts)))
-
-(defn dups-check [tpt pts]
-  (let [c (->>
-            pts
-            (map-eq-pts tpt)
-            (butlast)
-            (remove nil?)
-            (last))]
-    (if (nil? c)
-      0
-      c)))
+;(defn map-eq-pts [tpt pts]
+;  (vec (map (fn [{pt :pt c :c}] (if (= tpt pt) c nil)) pts)))
+;
+;(defn dups-check [tpt pts]
+;  (let [c (->>
+;            pts
+;            (map-eq-pts tpt)
+;            (butlast)
+;            (remove nil?)
+;            (last))]
+;    (if (nil? c)
+;      0
+;      c)))
 
 ;First, it will output a value indicating the color to paint the panel the robot is over:
 ;   0 means to paint the panel black, and 1 means to paint the panel white.
 ;Second, it will output a value indicating the direction the robot should turn:
 ;   0 means it should turn left 90 degrees, and 1 means it should turn right 90 degrees.
 
-(defn update-atom [coll p t]
-  (let [{:keys [pt h]} (last coll)
-        npt (new-point pt (new-heading h t))
-        new-2 [{:pt pt :c p} {:pt npt :h (new-heading h t) :c (dups-check npt coll)}]]
-    (into (vec (butlast coll)) new-2)))
-
-(swap! states update-atom 1 0)
-(swap! states update-atom 0 0)
-(swap! states update-atom 1 0)
-(swap! states update-atom 1 0)
+;(defn update-atom [coll p t]
+;  (let [{:keys [pt h]} (last coll)
+;        npt (new-point pt (new-heading h t))
+;        new-2 [{:pt pt :c p} {:pt npt :h (new-heading h t) :c (dups-check npt coll)}]]
+;    (into (vec (butlast coll)) new-2)))
+;
+;(swap! states update-atom 1 0)
+;(swap! states update-atom 0 0)
+;(swap! states update-atom 1 0)
+;(swap! states update-atom 1 0)
 
 ;(swap! states update-atom 0 1)
 ;(swap! states update-atom 1 0)
