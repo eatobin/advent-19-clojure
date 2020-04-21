2fd39a25843693135317db8161ff8e0e41708e78
Eric Tobin <eatobin@gmail.com>
2020-04-21 15:54:40 -0500

    count-repaints works

--- src/advent/day11.clj
+++ src/advent/day11.clj
@@ -6,52 +6,6 @@
 ;Second, it will output a value indicating the direction the robot should turn:
 ;   0 means it should turn left 90 degrees, and 1 means it should turn right 90 degrees.
 
-
-;For example, suppose the robot is about to start running. Drawing black panels as ., white panels as #,
-; and the robot pointing the direction it is facing (< ^ > v), the initial state and region near the robot looks like this:
-;
-;.....
-;.....
-;..^..
-;.....
-;.....
-;The panel under the robot (not visible here because a ^ is shown instead) is also black, and so any input instructions
-; at this point should be provided 0. Suppose the robot eventually outputs 1 (paint white) and then 0 (turn left).
-; After taking these actions and moving forward one panel, the region now looks like this:
-;
-;.....
-;.....
-;.<#..
-;.....
-;.....
-;Input instructions should still be provided 0. Next, the robot might output 0 (paint black) and then 0 (turn left):
-;
-;.....
-;.....
-;..#..
-;.v...
-;.....
-;After more outputs (1,0, 1,0):
-;
-;.....
-;.....
-;..^..
-;.##..
-;.....
-;The robot is now back where it started, but because it is now on a white panel, input instructions should be provided 1.
-; After several more outputs (0,1, 1,0, 1,0), the area looks like this:
-;
-;.....
-;..<#.
-;...#.
-;.##..
-;.....
-;Before you deploy the robot, you should probably have an estimate of the area it will cover:
-; specifically, you need to know the number of panels it paints at least once, regardless of color.
-; In the example above, the robot painted 6 panels at least once.
-; (It painted its starting panel twice, but that panel is still only counted once; it also never painted the panel it ended on.)
-
-
 ;part a
 (def tv (ic/make-tv "resources/day11.csv"))
 
@@ -119,23 +73,41 @@
         turned [{:pt new-point :h (new-heading h t) :c (dups-check new-point coll) :rp nil}]]
     (into (vec coll) turned)))
 
+(defn count-repaints [coll]
+  (count (filter #(some? (% :rp)) coll)))
+
+;[1 0]
 (swap! states paint-atom 1)
 (swap! states turn-atom 0)
 
+;[0 0]
 (swap! states paint-atom 0)
 (swap! states turn-atom 0)
 
+;[1 0]
 (swap! states paint-atom 1)
 (swap! states turn-atom 0)
 
+;[1 0]
 (swap! states paint-atom 1)
 (swap! states turn-atom 0)
 
+;[0 1]
 (swap! states paint-atom 0)
 (swap! states turn-atom 1)
 
+;[1 0]
 (swap! states paint-atom 1)
 (swap! states turn-atom 0)
 
+;[1 0]
 (swap! states paint-atom 1)
 (swap! states turn-atom 0)
+
+;6 repaints - count first just once
+
+;.....
+;..<#.
+;...#.
+;.##..
+;.....
