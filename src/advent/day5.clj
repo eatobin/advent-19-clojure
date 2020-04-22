3ed5376613931b54d632a7e1d504b5c1f359559b
Eric Tobin <eatobin@gmail.com>
2020-04-22 17:26:17 -0500

    progress

--- src/advent/day5.clj
+++ src/advent/day5.clj
@@ -4,14 +4,14 @@
 ;part a
 (def tv (ic/make-tv "resources/day5.csv"))
 
-(def answer ((ic/op-code {:input 1 :phase nil :pointer 0 :relative-base 0 :memory tv :stopped? false :recur? true}) :input))
+(def answer ((ic/op-code {:input 1 :output nil :phase nil :pointer 0 :relative-base 0 :memory tv :stopped? false :recur? true}) :input))
 
 (println answer) answer
 
 ;9025675
 
 ;part b
-(def answer-2 ((ic/op-code {:input 5 :phase nil :pointer 0 :relative-base 0 :memory tv :stopped? false :recur? true}) :input))
+(def answer-2 ((ic/op-code {:input 5 :output nil :phase nil :pointer 0 :relative-base 0 :memory tv :stopped? false :recur? true}) :input))
 
 (println answer-2)
 
