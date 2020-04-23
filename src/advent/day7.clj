68eaa8b21968dda30349fdbcccf30276db97a43d
Eric Tobin <eatobin@gmail.com>
2020-04-22 20:00:54 -0500

    progress

--- src/advent/day7.clj
+++ src/advent/day7.clj
@@ -51,21 +51,21 @@
                   5 (atom {:input nil :output nil :phase (phases 4) :pointer 0 :relative-base 0 :memory memory :stopped? false :recur? false})})]
          (map to-amps phases-vector))))
 
-;(defn runner [five-amps]
-;  (loop [amps five-amps
-;         current-amp-no 1
-;         next-amp-no (+ 1 (mod current-amp-no 5))]
-;    (if (and (= 5 current-amp-no) (:stopped? @(amps current-amp-no)))
-;      (:input @(amps current-amp-no))
-;      (let [op-this (atom (swap! (amps current-amp-no) ic/op-code))
-;            op-next (atom (swap! (amps next-amp-no) assoc :input (:input @op-this)))]
-;        (recur
-;          (assoc amps current-amp-no op-this next-amp-no op-next)
-;          next-amp-no
-;          (+ 1 (mod next-amp-no 5)))))))
-;
-;(def answer-2 (apply max (vec (map runner (to-amps-vector possibles-2 tv)))))
-;
-;(println answer-2)
+(defn runner [five-amps]
+  (loop [amps five-amps
+         current-amp-no 1
+         next-amp-no (+ 1 (mod current-amp-no 5))]
+    (if (and (= 5 current-amp-no) (:stopped? @(amps current-amp-no)))
+      (:output @(amps current-amp-no))
+      (let [op-this (atom (swap! (amps current-amp-no) ic/op-code))
+            op-next (atom (swap! (amps next-amp-no) assoc :input (:output @op-this)))]
+        (recur
+          (assoc amps current-amp-no op-this next-amp-no op-next)
+          next-amp-no
+          (+ 1 (mod next-amp-no 5)))))))
+
+(def answer-2 (apply max (vec (map runner (to-amps-vector possibles-2 tv)))))
+
+(println answer-2)
 
 ;35993240
