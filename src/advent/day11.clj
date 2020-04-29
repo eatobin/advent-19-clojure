c16396816d65bb04d6e4486783e81b20ad5f9cf3
Eric Tobin <eatobin@gmail.com>
2020-04-29 17:08:39 -0500

    part a done!!!

--- src/advent/day11.clj
+++ src/advent/day11.clj
@@ -1,16 +1,8 @@
 (ns advent.day11
   (:require [advent.intcode :as ic]))
 
-;First, it will output a value indicating the color to paint the panel the robot is over:
-;   0 means to paint the panel black, and 1 means to paint the panel white.
-;Second, it will output a value indicating the direction the robot should turn:
-;   0 means it should turn left 90 degrees, and 1 means it should turn right 90 degrees.
-
 ;part a
-;(def tv (ic/make-tv "resources/day11.csv"))
-(def tv (ic/make-tv "resources/day11-test.csv"))
-
-(ic/op-code {:input 0 :output nil :phase nil :pointer 0 :relative-base 0 :memory tv :stopped? false :recur? true})
+(def tv (ic/make-tv "resources/day11.csv"))
 
 (defn new-heading [heading turn]
   (cond
@@ -36,10 +28,6 @@
     (and (= c 1) (= p 0)) 0
     (and (= c 1) (= p 1)) 1
     (and (= c 0) (= p 0)) 0))
-
-(def state {:pt {:x 0 :y 0} :h :n :c 0 :rp nil})
-
-(def states (atom [{:pt {:x 0 :y 0} :h :n :c 0 :rp nil}]))
 
 (def visits (atom [{:pt {:x 0 :y 0} :h :n :c 0 :rp nil}]))
 
@@ -74,52 +62,11 @@
         turned [{:pt new-point :h (new-heading h t) :c (dups-check new-point coll) :rp nil}]]
     (into (vec coll) turned)))
 
-(defn count-repaints [coll]
-  (count (filter #(some? (% :rp)) coll)))
-
-;[1 0]
-(swap! states paint-atom 1)
-(swap! states turn-atom 0)
-
-;[0 0]
-(swap! states paint-atom 0)
-(swap! states turn-atom 0)
-
-;[1 0]
-(swap! states paint-atom 1)
-(swap! states turn-atom 0)
-
-;[1 0]
-(swap! states paint-atom 1)
-(swap! states turn-atom 0)
-
-;Back to start
-
-;[0 1]
-(swap! states paint-atom 0)
-(swap! states turn-atom 1)
-
-;[1 0]
-(swap! states paint-atom 1)
-(swap! states turn-atom 0)
-
-;[1 0]
-(swap! states paint-atom 1)
-(swap! states turn-atom 0)
-
-;6 repaints
-
-;.....
-;..<#.
-;...#.
-;.##..
-;.....
-
 (defn runner [visits oc]
   (loop [c ((last @visits) :c)]
     (atom (reset! oc (ic/op-code (assoc @oc :input c))))
     (if (@oc :stopped?)
-      [@oc @visits]
+      (count (distinct (map :pt (vec (remove #(nil? (% :rp)) @visits)))))
       (do
         (atom (reset! visits (paint-atom @visits (@oc :output))))
         (atom (swap! oc ic/op-code))
@@ -127,8 +74,8 @@
         (recur
           ((last @visits) :c))))))
 
-(def run-done (runner visits oc))
-
-(def answer (count (distinct (map :pt (vec (remove #(nil? (% :rp)) @visits))))))
+(def answer (runner visits oc))
 
 (println answer)
+
+;1771
