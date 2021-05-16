8fabd5e0a46415c4a7b287470ffea74ed1f24748
Eric Tobin <eatobin@gmail.com>
2021-05-16 18:40:38 -0500

    lisp format

--- src/advent/day12.clj
+++ src/advent/day12.clj
@@ -1,10 +1,13 @@
 (ns advent.day12
-  (:require [clojure.data.csv :as csv]
-            [clojure.java.io :as io]
-            [clojure.string :as str]))
+  (:require
+   [clojure.data.csv :as csv]
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
 
 ;part a
 (def tv (let [vcs (with-open [reader (io/reader "resources/day12.csv")]
                     (doall
-                      (csv/read-csv reader)))]
+                     (csv/read-csv reader)))]
           (vec (map vec (map #(map str/trim %) vcs)))))
+
+(println tv)
