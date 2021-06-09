6879a57600bcc481cb6d3cce76356f56e7529539
Eric Tobin <eatobin@gmail.com>
2021-06-09 17:55:36 -0500

    progress

--- src/advent/day12.clj
+++ src/advent/day12.clj
@@ -1,13 +1,34 @@
 (ns advent.day12
   (:require
-   [clojure.data.csv :as csv]
-   [clojure.java.io :as io]
-   [clojure.string :as str]))
+    [clojure.data.csv :as csv]
+    [clojure.java.io :as io]
+    [clojure.string :as str]))
 
 ;part a
+
+(def moon-template
+  {:i {:pos {:x 0, :y 0, :z 0},
+       :vel {:x 0, :y 0, :z 0}}
+   :e {:pos {:x 0, :y 0, :z 0},
+       :vel {:x 0, :y 0, :z 0}}
+   :g {:pos {:x 0, :y 0, :z 0},
+       :vel {:x 0, :y 0, :z 0}}
+   :c {:pos {:x 0, :y 0, :z 0},
+       :vel {:x 0, :y 0, :z 0}}})
+
 (def tv (let [vcs (with-open [reader (io/reader "resources/day12.csv")]
                     (doall
-                     (csv/read-csv reader)))]
+                      (csv/read-csv reader)))]
           (vec (map vec (map #(map str/trim %) vcs)))))
+
+
+
+(def vcs (with-open [reader (io/reader "resources/day12.csv")]
+           (doall
+             (csv/read-csv reader))))
+
+(def i (first vcs))
+
+(zipmap [:x :y :z] (map #(Integer/parseInt %)(map #(get % 1) (map #(str/split % #"=") (map #(str/replace % ">" "") i)))))
 
 (println tv)
