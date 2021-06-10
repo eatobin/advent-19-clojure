f18abfc39cb61f9af2698124a51fbbf704023916
Eric Tobin <eatobin@gmail.com>
2021-06-09 21:52:52 -0500

    cider format...

--- src/advent/day12.clj
+++ src/advent/day12.clj
@@ -1,22 +1,22 @@
 (ns advent.day12
   (:require
-    [clojure.data.csv :as csv]
-    [clojure.java.io :as io]
-    [clojure.string :as str]))
+   [clojure.data.csv :as csv]
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
 
 ;part a
 (def vcs (with-open [reader (io/reader "resources/day12.csv")]
            (doall
-             (csv/read-csv reader))))
+            (csv/read-csv reader))))
 
 (defn make-moon-map [moon-pos]
   (->>
-    moon-pos
-    (map #(str/replace % ">" ""))
-    (map #(str/split % #"="))
-    (map #(get % 1))
-    (map #(Integer/parseInt %))
-    (zipmap [:x :y :z])))
+   moon-pos
+   (map #(str/replace % ">" ""))
+   (map #(str/split % #"="))
+   (map #(get % 1))
+   (map #(Integer/parseInt %))
+   (zipmap [:x :y :z])))
 
 (def moon-maps (into [] (map make-moon-map vcs)))
 
@@ -32,10 +32,10 @@
 
 (def moon-meld
   (->
-    moon-template
-    (assoc-in [:i :pos] (get moon-maps 0))
-    (assoc-in [:e :pos] (get moon-maps 1))
-    (assoc-in [:g :pos] (get moon-maps 2))
-    (assoc-in [:c :pos] (get moon-maps 3))))
+   moon-template
+   (assoc-in [:i :pos] (get moon-maps 0))
+   (assoc-in [:e :pos] (get moon-maps 1))
+   (assoc-in [:g :pos] (get moon-maps 2))
+   (assoc-in [:c :pos] (get moon-maps 3))))
 
 moon-meld
