174279e0c7331fcf0784779f51fde10e4593108b
Eric Tobin <eatobin@gmail.com>
2021-06-09 21:04:49 -0500

    progress

--- src/advent/day12.clj
+++ src/advent/day12.clj
@@ -5,6 +5,20 @@
     [clojure.string :as str]))
 
 ;part a
+(def vcs (with-open [reader (io/reader "resources/day12.csv")]
+           (doall
+             (csv/read-csv reader))))
+
+(defn make-moon-map [moon-pos]
+  (->>
+    moon-pos
+    (map #(str/replace % ">" ""))
+    (map #(str/split % #"="))
+    (map #(get % 1))
+    (map #(Integer/parseInt %))
+    (zipmap [:x :y :z])))
+
+(def moon-maps (map make-moon-map vcs))
 
 (def moon-template
   {:i {:pos {:x 0, :y 0, :z 0},
@@ -16,19 +30,22 @@
    :c {:pos {:x 0, :y 0, :z 0},
        :vel {:x 0, :y 0, :z 0}}})
 
-(def tv (let [vcs (with-open [reader (io/reader "resources/day12.csv")]
-                    (doall
-                      (csv/read-csv reader)))]
-          (vec (map vec (map #(map str/trim %) vcs)))))
+;(def tv (let [vcs (with-open [reader (io/reader "resources/day12.csv")]
+;                    (doall
+;                      (csv/read-csv reader)))]
+;          (vec (map vec (map #(map str/trim %) vcs)))))
 
 
 
-(def vcs (with-open [reader (io/reader "resources/day12.csv")]
-           (doall
-             (csv/read-csv reader))))
 
-(def i (first vcs))
 
-(zipmap [:x :y :z] (map #(Integer/parseInt %)(map #(get % 1) (map #(str/split % #"=") (map #(str/replace % ">" "") i)))))
+;(def i (first vcs))
 
-(println tv)
+;(defn make-moon-map [moon-pos]
+;  (zipmap [:x :y :z] (map #(Integer/parseInt %)(map #(get % 1) (map #(str/split % #"=") (map #(str/replace % ">" "") moon-pos))))))
+
+
+
+;(zipmap [:x :y :z] (map #(Integer/parseInt %) (map #(get % 1) (map #(str/split % #"=") (map #(str/replace % ">" "") i)))))
+;
+;(println tv)
