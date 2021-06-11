d778a647c3fcc9cbca6a2e3bbd2c2867c3bb1887
Eric Tobin <eatobin@gmail.com>
2021-06-11 18:33:15 -0500

    kibit day03

--- src/advent/day03.clj
+++ src/advent/day03.clj
@@ -11,13 +11,11 @@
 
 (def both (with-open [reader (io/reader "resources/day03.csv")]
             (doall
-             (csv/read-csv reader))))
+              (csv/read-csv reader))))
 
-(def red (->> (first both)
-              (into [])))
+(def red (vec (first both)))
 
-(def blue (->> (second both)
-               (into [])))
+(def blue (vec (second both)))
 
 (defn direction [unit]
   (subs unit 0 1))
@@ -28,15 +26,15 @@
 (defn make-path [unit [a b]]
   (let [direction (direction unit)
         distance (distance unit)]
-    (into [] (case direction
-               "R" (for [x (range a (+ distance a))]
-                     [x b])
-               "U" (for [y (range b (+ distance b))]
-                     [a y])
-               "L" (for [x (range a (+ (- distance) a) -1)]
-                     [x b])
-               "D" (for [y (range b (+ (- distance) b) -1)]
-                     [a y])))))
+    (vec (case direction
+           "R" (for [x (range a (+ distance a))]
+                 [x b])
+           "U" (for [y (range b (+ distance b))]
+                 [a y])
+           "L" (for [x (range a (+ (- distance) a) -1)]
+                 [x b])
+           "D" (for [y (range b (+ (- distance) b) -1)]
+                 [a y])))))
 
 (defn make-paths [units start]
   (loop [units units
@@ -45,16 +43,16 @@
     (if (empty? units)
       path
       (recur
-       (rest units)
-       (last (make-path (first units) start))
-       (into [] (concat path (rest (make-path (first units) start))))))))
+        (rest units)
+        (last (make-path (first units) start))
+        (vec (concat path (rest (make-path (first units) start))))))))
 
 (defn abs-dist [[x y]]
   (+ (math/abs x) (math/abs y)))
 
 (def answer
-  (let [red-set (into #{} (make-paths red [0 0]))
-        blue-set (into #{} (make-paths blue [0 0]))
+  (let [red-set (set (make-paths red [0 0]))
+        blue-set (set (make-paths blue [0 0]))
         red-blue-intersect (s/intersection red-set blue-set)]
     (apply min (map abs-dist (disj red-blue-intersect [0 0])))))
 
@@ -65,8 +63,8 @@
 ;part b
 (def blue-v (make-paths blue [0 0]))
 (def red-v (make-paths red [0 0]))
-(def blue-set (into #{} blue-v))
-(def red-set (into #{} red-v))
+(def blue-set (set blue-v))
+(def red-set (set red-v))
 (def blue-v-i (map-indexed vector blue-v))
 (def red-v-i (map-indexed vector red-v))
 (def red-blue-intersect (clojure.set/intersection red-set blue-set))
