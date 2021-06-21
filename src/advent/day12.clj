ec4d370b9d2d383c97b8b002aad7272137dc7ddb
Eric Tobin <eatobin@gmail.com>
2021-06-20 20:55:48 -0500

    good progress

--- src/advent/day12.clj
+++ src/advent/day12.clj
@@ -77,8 +77,16 @@
         moon-1 (get moon-vec-1 1)
         axis (get moon-vec-0 2)]
     (cond
-      (> (math/abs moon-pos-0) (math/abs moon-pos-1)) (do (swap! moon-meld update-in [moon-0 :vel axis] dec) (swap! moon-meld update-in [moon-1 :vel axis] inc))
-      (> (math/abs moon-pos-1) (math/abs moon-pos-0)) (do (swap! moon-meld update-in [moon-1 :vel axis] dec) (swap! moon-meld update-in [moon-0 :vel axis] inc))
-      :else [moon-pos-0 moon-pos-1 moon-0 moon-1 axis])))
+      (> (math/abs moon-pos-0) (math/abs moon-pos-1)) (do (swap! moon-meld update-in [moon-0 :vel axis] dec)
+                                                          (swap! moon-meld update-in [moon-1 :vel axis] inc))
+      (> (math/abs moon-pos-1) (math/abs moon-pos-0)) (do (swap! moon-meld update-in [moon-1 :vel axis] dec)
+                                                          (swap! moon-meld update-in [moon-0 :vel axis] inc))
+      (and (= (math/abs moon-pos-0) (math/abs moon-pos-1))
+           (pos-int? moon-pos-0)) (do (swap! moon-meld update-in [moon-0 :vel axis] dec)
+                                      (swap! moon-meld update-in [moon-1 :vel axis] inc))
+      (and (= (math/abs moon-pos-0) (math/abs moon-pos-1))
+           (pos-int? moon-pos-1)) (do (swap! moon-meld update-in [moon-1 :vel axis] dec)
+                                      (swap! moon-meld update-in [moon-0 :vel axis] inc))
+      :else [moon-vec-0 moon-vec-1])))
 
 moon-meld
