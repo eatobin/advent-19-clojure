67d39e8effa426e406b65470ce77eaed073508e3
Eric Tobin <eatobin@gmail.com>
2021-06-27 09:56:01 -0500

    atoms

--- src/advent/day12.clj
+++ src/advent/day12.clj
@@ -21,66 +21,62 @@
 (def moon-maps (into [] (map make-moon-map vcs)))
 
 (def moon-template
-  [[[0 0 0] [0 0 0]]
-   [[0 0 0] [0 0 0]]
-   [[0 0 0] [0 0 0]]
-   [[0 0 0] [0 0 0]]])
+  [(atom [[0 0 0] [0 0 0]])
+   (atom [[0 0 0] [0 0 0]])
+   (atom [[0 0 0] [0 0 0]])
+   (atom [[0 0 0] [0 0 0]])])
 
+(do
+  (swap! (get moon-template 0) assoc-in [0] (get moon-maps 0))
+  (swap! (get moon-template 1) assoc-in [0] (get moon-maps 1))
+  (swap! (get moon-template 2) assoc-in [0] (get moon-maps 2))
+  (swap! (get moon-template 3) assoc-in [0] (get moon-maps 3)))
 
-
-(def moon-meld
-  (atom (->
-          moon-template
-          (assoc-in [0 0] (get moon-maps 0))
-          (assoc-in [1 0] (get moon-maps 1))
-          (assoc-in [2 0] (get moon-maps 2))
-          (assoc-in [3 0] (get moon-maps 3)))))
-
-(def candidates
-  [[[0 0 0] [1 0 0]]
-   [[0 0 0] [2 0 0]]
-   [[0 0 0] [3 0 0]]
-   [[1 0 0] [2 0 0]]
-   [[1 0 0] [3 0 0]]
-   [[2 0 0] [3 0 0]]
-
-   [[0 0 1] [1 0 1]]
-   [[0 0 1] [2 0 1]]
-   [[0 0 1] [3 0 1]]
-   [[1 0 1] [2 0 1]]
-   [[1 0 1] [3 0 1]]
-   [[2 0 1] [3 0 1]]
-
-   [[0 0 2] [1 0 2]]
-   [[0 0 2] [2 0 2]]
-   [[0 0 2] [3 0 2]]
-   [[1 0 2] [2 0 2]]
-   [[1 0 2] [3 0 2]]
-   [[2 0 2] [3 0 2]]])
-
-(defn moon-getter [moon]
-  [(get-in @moon-meld moon) (moon 0) (moon 2)])
-
-(defn moons-pair [moons]
-  (vec (map moon-getter moons)))
-
-(def all-candidates
-  (vec (map moons-pair candidates)))
-
-(defn gravity-update [all-candidates]
-  (for [[moon-vec-0 moon-vec-1] all-candidates
-        :let [moon-pos-0 (get moon-vec-0 0)
-              moon-pos-1 (get moon-vec-1 0)
-              moon-0 (get moon-vec-0 1)
-              moon-1 (get moon-vec-1 1)
-              axis (get moon-vec-0 2)
-              moon-0-velocity (cond (> moon-pos-0 moon-pos-1) -1
-                                    (> moon-pos-1 moon-pos-0) 1
-                                    :else 0)
-              moon-1-velocity (cond (> moon-pos-1 moon-pos-0) -1
-                                    (> moon-pos-0 moon-pos-1) 1
-                                    :else 0)]]
-    (do (swap! moon-meld update-in [moon-0 1 axis] + moon-0-velocity)
-        (swap! moon-meld update-in [moon-1 1 axis] + moon-1-velocity))))
-
-(gravity-update all-candidates)
+;(def candidates
+;  [[[0 0 0] [1 0 0]]
+;   [[0 0 0] [2 0 0]]
+;   [[0 0 0] [3 0 0]]
+;   [[1 0 0] [2 0 0]]
+;   [[1 0 0] [3 0 0]]
+;   [[2 0 0] [3 0 0]]
+;
+;   [[0 0 1] [1 0 1]]
+;   [[0 0 1] [2 0 1]]
+;   [[0 0 1] [3 0 1]]
+;   [[1 0 1] [2 0 1]]
+;   [[1 0 1] [3 0 1]]
+;   [[2 0 1] [3 0 1]]
+;
+;   [[0 0 2] [1 0 2]]
+;   [[0 0 2] [2 0 2]]
+;   [[0 0 2] [3 0 2]]
+;   [[1 0 2] [2 0 2]]
+;   [[1 0 2] [3 0 2]]
+;   [[2 0 2] [3 0 2]]])
+;
+;(defn moon-getter [moon]
+;  [(get-in @moon-meld moon) (moon 0) (moon 2)])
+;
+;(defn moons-pair [moons]
+;  (vec (map moon-getter moons)))
+;
+;(def all-candidates
+;  (vec (map moons-pair candidates)))
+;
+;(defn gravity-update [all-candidates]
+;  (for [[moon-vec-0 moon-vec-1] all-candidates
+;        :let [moon-pos-0 (get moon-vec-0 0)
+;              moon-pos-1 (get moon-vec-1 0)
+;              moon-0 (get moon-vec-0 1)
+;              moon-1 (get moon-vec-1 1)
+;              axis (get moon-vec-0 2)
+;              moon-0-velocity (cond (> moon-pos-0 moon-pos-1) -1
+;                                    (> moon-pos-1 moon-pos-0) 1
+;                                    :else 0)
+;              moon-1-velocity (cond (> moon-pos-1 moon-pos-0) -1
+;                                    (> moon-pos-0 moon-pos-1) 1
+;                                    :else 0)]]
+;    (do (swap! moon-meld update-in [moon-0 1 axis] + moon-0-velocity)
+;        (swap! moon-meld update-in [moon-1 1 axis] + moon-1-velocity))))
+;
+;(gravity-update all-candidates)
