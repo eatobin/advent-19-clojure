b61f4a8428da2bf5307953945e129e90aca972c8
Eric Tobin <eatobin@gmail.com>
2021-07-05 13:14:29 -0500

    works!!!

--- src/advent/day12.clj
+++ src/advent/day12.clj
@@ -70,68 +70,24 @@
 (defn moons-pair [moons]
   (vec (map moon-getter moons)))
 
-;(def all-candidates
-;  (atom (vec (map moons-pair candidates))))
-;
-;(defn update-candidates []
-;  (reset! all-candidates (vec (map moons-pair candidates))))
-;
-(defn apply-gravity [candidates]
-  (for [[moon-vec-0 moon-vec-1] (vec (map moons-pair candidates))
-        :let [moon-pos-0 (get moon-vec-0 2)
-              moon-pos-1 (get moon-vec-1 2)
-              moon-0 (get moon-vec-0 0)
-              moon-1 (get moon-vec-1 0)
-              axis (get moon-vec-0 1)
-              moon-0-velocity (cond (> moon-pos-0 moon-pos-1) -1
-                                    (> moon-pos-1 moon-pos-0) 1
-                                    :else 0)
-              moon-1-velocity (cond (> moon-pos-1 moon-pos-0) -1
-                                    (> moon-pos-0 moon-pos-1) 1
-                                    :else 0)]]
-    [(swap! moon-meld update-in [moon-0 :vel axis] + moon-0-velocity) (swap! moon-meld update-in [moon-1 :vel axis] + moon-1-velocity)]))
+(defn apply-gravity []
+  (doseq [[moon-vec-0 moon-vec-1] (vec (map moons-pair candidates))
+          :let [moon-pos-0 (get moon-vec-0 2)
+                moon-pos-1 (get moon-vec-1 2)
+                moon-0 (get moon-vec-0 0)
+                moon-1 (get moon-vec-1 0)
+                axis (get moon-vec-0 1)
+                moon-0-velocity (cond (> moon-pos-0 moon-pos-1) -1
+                                      (> moon-pos-1 moon-pos-0) 1
+                                      :else 0)
+                moon-1-velocity (cond (> moon-pos-1 moon-pos-0) -1
+                                      (> moon-pos-0 moon-pos-1) 1
+                                      :else 0)]]
+    (swap! moon-meld update-in [moon-0 :vel axis] + moon-0-velocity)
+    (swap! moon-meld update-in [moon-1 :vel axis] + moon-1-velocity)))
 
-(def a (atom [0 0]))
-(doseq [x (range 6)
-        :when (odd? x)
-        :let [y (* x x)]]
-  (reset! a (assoc @a 0 y))
-  (reset! a (assoc @a 1 (dec y))))
-
-;(defn apply-gravity [candidates]
-;  (for [[moon-vec-0 moon-vec-1] (vec (map moons-pair candidates))
-;        :let [moon-pos-0 (get moon-vec-0 2)
-;              moon-pos-1 (get moon-vec-1 2)
-;              moon-0 (get moon-vec-0 0)
-;              moon-1 (get moon-vec-1 0)
-;              axis (get moon-vec-0 1)
-;              moon-0-velocity (cond (> moon-pos-0 moon-pos-1) -1
-;                                    (> moon-pos-1 moon-pos-0) 1
-;                                    :else 0)
-;              moon-1-velocity (cond (> moon-pos-1 moon-pos-0) -1
-;                                    (> moon-pos-0 moon-pos-1) 1
-;                                    :else 0)]]
-;    ))
-
-;(->
-;  candidates
-;  (apply-gravity))
-
-;(defn apply-gravities []
-;  (apply-gravity all-candidates))
-;
-;(defn apply-velocities []
-;  (doseq [[_ {name :name {x-vel :x, y-vel :y, z-vel :z} :vel}] @moon-meld]
-;    (swap! moon-meld update-in [name :pos :x] + x-vel)
-;    (swap! moon-meld update-in [name :pos :y] + y-vel)
-;    (swap! moon-meld update-in [name :pos :z] + z-vel)))
-;
-;(def steps [apply-gravities apply-velocities])
-;
-;(apply-gravities)
-;(apply-velocities)
-;(update-candidates)
-;
-;(doall (map #(%) [apply-gravities apply-velocities]))
-;
-;(deref moon-meld)
+(defn apply-velocities []
+  (doseq [[_ {name :name {x-vel :x, y-vel :y, z-vel :z} :vel}] @moon-meld]
+    (swap! moon-meld update-in [name :pos :x] + x-vel)
+    (swap! moon-meld update-in [name :pos :y] + y-vel)
+    (swap! moon-meld update-in [name :pos :z] + z-vel)))
