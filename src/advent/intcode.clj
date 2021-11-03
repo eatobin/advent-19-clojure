e11af097c1ca8a5225fa063c000a64cda76be6df
Eric Tobin <eatobin@gmail.com>
2021-11-03 16:31:50 -0500

    better at-address

--- src/advent/intcode.clj
+++ src/advent/intcode.clj
@@ -324,9 +324,13 @@
         "Unknown opcode"))))
 
 (defn at-address [{:keys [pointer a-b-c memory relative-base]}]
-  (+
-    (memory (+ a-b-c pointer))
-    relative-base))
+  (let [a-b-c (cond
+                (= :c a-b-c) 1
+                (= :b a-b-c) 2
+                :else 3)]
+    (+
+      (memory (+ a-b-c pointer))
+      relative-base)))
 
 (defn at-address-is-pointer [{:keys [pointer a-b-c memory relative-base]}]
   (get memory (at-address {:pointer       pointer
