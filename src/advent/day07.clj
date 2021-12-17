1631ebf575f033c6ff33d3ccc4f408a884ddf7cb
Eric Tobin <eatobin@gmail.com>
2021-12-17 15:41:06 -0600

    start day07

--- src/advent/day07.clj
+++ src/advent/day07.clj
@@ -1,8 +1,148 @@
 (ns advent.day07
-  (:require [advent.intcode :as ic]))
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
+
+; ABCDE
+;  1002
+
+; a- b- or c- = left-to-right position after 2 digit opcode
+; -p- -i- or -r- = position, immediate or relative mode
+; -r or -w = read or write
+
+(defn make-tv [file]
+  (->> (first (with-open [reader (io/reader file)]
+                (doall
+                  (csv/read-csv reader))))
+       (map #(Long/parseLong %))
+       (into [])))
+
+(def OFFSET-C 1)
+(def OFFSET-B 2)
+(def OFFSET-A 3)
+
+(defn char-to-int [char-as-byte]
+  (if (or (< char-as-byte 48)
+          (> char-as-byte 57))
+    "Char is not an integer"
+    (- char-as-byte 48)))
+
+(defn pad-5 [instruction]
+  (zipmap [:a :b :c :d :e]
+          (for [character (format "%05d" instruction)]
+            (char-to-int (byte character)))))
+
+(defn get-or-else [pointer OFFSET-X memory]
+  (if (> (+ pointer OFFSET-X)
+         (- (count memory) 1))
+    0
+    (memory (memory (+ pointer OFFSET-X)))))
+
+(defn a-param [{:keys [instruction pointer memory]}]
+  (case (instruction :a)
+    ; a-p-w
+    0 (memory (+ pointer OFFSET-A))))
+
+(defn b-param [{:keys [instruction pointer memory]}]
+  (case (instruction :b)
+    ; b-p-r
+    0 (get-or-else pointer OFFSET-B memory)
+    ; b-i-r
+    1 (memory (+ pointer OFFSET-B))))
+
+(defn c-param [{:keys [instruction pointer memory]}]
+  (if (= 3 (instruction :e))
+    (case (instruction :c)
+      ; c-p-w
+      0 (memory (+ pointer OFFSET-C)))
+    (case (instruction :c)
+      ; c-p-r
+      0 (get-or-else pointer OFFSET-C memory)
+      ; c-i-r
+      1 (memory (+ pointer OFFSET-C)))))
+
+(defn op-code [{:keys [input output pointer memory]}]
+  (let [instruction (pad-5 (memory pointer))]
+    (if (= 9 (instruction :d))
+      {:input input :output output :pointer pointer :memory memory}
+      (case (instruction :e)
+        1 (recur
+            {:input   input
+             :output  output
+             :pointer (+ 4 pointer)
+             :memory  (assoc
+                        memory
+                        (a-param {:instruction instruction :pointer pointer :memory memory})
+                        (+ (c-param {:instruction instruction :pointer pointer :memory memory})
+                           (b-param {:instruction instruction :pointer pointer :memory memory})))})
+        2 (recur
+            {:input   input
+             :output  output
+             :pointer (+ 4 pointer)
+             :memory  (assoc
+                        memory
+                        (a-param {:instruction instruction :pointer pointer :memory memory})
+                        (* (c-param {:instruction instruction :pointer pointer :memory memory})
+                           (b-param {:instruction instruction :pointer pointer :memory memory})))})
+        3 (recur
+            {:input   input
+             :output  output
+             :pointer (+ 2 pointer)
+             :memory  (assoc
+                        memory
+                        (c-param {:instruction instruction :pointer pointer :memory memory})
+                        input)})
+        4 (recur
+            {:input   input
+             :output  (c-param {:instruction instruction :pointer pointer :memory memory})
+             :pointer (+ 2 pointer)
+             :memory  memory})
+        5 (recur
+            {:input   input
+             :output  output
+             :pointer (if (= 0 (c-param {:instruction instruction :pointer pointer :memory memory}))
+                        (+ 3 pointer)
+                        (b-param {:instruction instruction :pointer pointer :memory memory}))
+             :memory  memory})
+        6 (recur
+            {:input   input
+             :output  output
+             :pointer (if (not= 0 (c-param {:instruction instruction :pointer pointer :memory memory}))
+                        (+ 3 pointer)
+                        (b-param {:instruction instruction :pointer pointer :memory memory}))
+             :memory  memory})
+        7 (recur
+            {:input   input
+             :output  output
+             :pointer (+ 4 pointer)
+             :memory  (if (< (c-param {:instruction instruction :pointer pointer :memory memory})
+                             (b-param {:instruction instruction :pointer pointer :memory memory}))
+                        (assoc
+                          memory
+                          (a-param {:instruction instruction :pointer pointer :memory memory})
+                          1)
+                        (assoc
+                          memory
+                          (a-param {:instruction instruction :pointer pointer :memory memory})
+                          0))})
+        8 (recur
+            {:input   input
+             :output  output
+             :pointer (+ 4 pointer)
+             :memory  (if (= (c-param {:instruction instruction :pointer pointer :memory memory})
+                             (b-param {:instruction instruction :pointer pointer :memory memory}))
+                        (assoc
+                          memory
+                          (a-param {:instruction instruction :pointer pointer :memory memory})
+                          1)
+                        (assoc
+                          memory
+                          (a-param {:instruction instruction :pointer pointer :memory memory})
+                          0))})
+        "Unknown opcode"))))
+
 
 ;part a
-(def tv (ic/make-tv "resources/day07.csv"))
+(def tv (make-tv "resources/day07.csv"))
 
 (def possibles (for [a (range 0 5)
                      b (range 0 5)
@@ -13,42 +153,42 @@
                  [a b c d e]))
 
 (defn pass [[a b c d e] i-code]
-  ((ic/op-code {:input         ((ic/op-code {:input         ((ic/op-code {:input         ((ic/op-code {:input         ((ic/op-code {:input         0
-                                                                                                                                    :output        nil
-                                                                                                                                    :phase         a
-                                                                                                                                    :pointer       0
-                                                                                                                                    :relative-base 0
-                                                                                                                                    :memory        i-code
-                                                                                                                                    :stopped?      false
-                                                                                                                                    :recur?        true}) :output)
-                                                                                                       :output        nil
-                                                                                                       :phase         b
-                                                                                                       :pointer       0
-                                                                                                       :relative-base 0
-                                                                                                       :memory        i-code
-                                                                                                       :stopped?      false
-                                                                                                       :recur?        true}) :output)
-                                                                          :output        nil
-                                                                          :phase         c
-                                                                          :pointer       0
-                                                                          :relative-base 0
-                                                                          :memory        i-code
-                                                                          :stopped?      false
-                                                                          :recur?        true}) :output)
-                                             :output        nil
-                                             :phase         d
-                                             :pointer       0
-                                             :relative-base 0
-                                             :memory        i-code
-                                             :stopped?      false
-                                             :recur?        true}) :output)
-                :output        nil
-                :phase         e
-                :pointer       0
-                :relative-base 0
-                :memory        i-code
-                :stopped?      false
-                :recur?        true}) :output))
+  ((op-code {:input         ((op-code {:input         ((op-code {:input         ((op-code {:input         ((op-code {:input         0
+                                                                                                                     :output        nil
+                                                                                                                     :phase         a
+                                                                                                                     :pointer       0
+                                                                                                                     :relative-base 0
+                                                                                                                     :memory        i-code
+                                                                                                                     :stopped?      false
+                                                                                                                     :recur?        true}) :output)
+                                                                                           :output        nil
+                                                                                           :phase         b
+                                                                                           :pointer       0
+                                                                                           :relative-base 0
+                                                                                           :memory        i-code
+                                                                                           :stopped?      false
+                                                                                           :recur?        true}) :output)
+                                                                 :output        nil
+                                                                 :phase         c
+                                                                 :pointer       0
+                                                                 :relative-base 0
+                                                                 :memory        i-code
+                                                                 :stopped?      false
+                                                                 :recur?        true}) :output)
+                                       :output        nil
+                                       :phase         d
+                                       :pointer       0
+                                       :relative-base 0
+                                       :memory        i-code
+                                       :stopped?      false
+                                       :recur?        true}) :output)
+             :output        nil
+             :phase         e
+             :pointer       0
+             :relative-base 0
+             :memory        i-code
+             :stopped?      false
+             :recur?        true}) :output))
 
 (defn passes [i-code]
   (map #(pass % i-code) possibles))
@@ -83,7 +223,7 @@
          next-amp-no (+ 1 (mod current-amp-no 5))]
     (if (and (= 5 current-amp-no) (:stopped? @(amps current-amp-no)))
       (:output @(amps current-amp-no))
-      (do (swap! (amps current-amp-no) ic/op-code)
+      (do (swap! (amps current-amp-no) op-code)
           (swap! (amps next-amp-no) assoc :input (:output @(amps current-amp-no)))
           (recur
             (assoc amps current-amp-no (amps current-amp-no) next-amp-no (amps next-amp-no))
