4857db1d530920fd9c07ce2b115b8f87134aecea
Eric Tobin <eatobin@gmail.com>
2021-12-29 10:04:19 -0600

    map returns - and works!!!

--- src/advent/day09.clj
+++ src/advent/day09.clj
@@ -206,35 +206,35 @@
                :recur?        recur?})
           "Unknown opcode")))))
 
-;;part a
-;(def tv (make-tv "resources/day09.csv"))
-;
-;
-;(def answer ((op-code {:input         1
-;                       :output        0
-;                       :phase         -1
-;                       :pointer       0
-;                       :relative-base 0
-;                       :memory        tv
-;                       :stopped?      false
-;                       :recur?        true})
-;             :output))
-;
-;(println answer)
-;
+;; part a
+(def tv (make-tv "resources/day09.csv"))
+
+
+(def answer ((op-code {:input         1
+                       :output        0
+                       :phase         -1
+                       :pointer       0
+                       :relative-base 0
+                       :memory        tv
+                       :stopped?      false
+                       :recur?        true})
+             :output))
+
+(println answer)
+
 ;; 3780860499
-;
+
 ;; part b
-;(def answer-2 ((op-code {:input         2
-;                         :output        0
-;                         :phase         -1
-;                         :pointer       0
-;                         :relative-base 0
-;                         :memory        tv
-;                         :stopped?      false
-;                         :recur?        true})
-;               :output))
-;
-;(println answer-2)
-;
+(def answer-2 ((op-code {:input         2
+                         :output        0
+                         :phase         -1
+                         :pointer       0
+                         :relative-base 0
+                         :memory        tv
+                         :stopped?      false
+                         :recur?        true})
+               :output))
+
+(println answer-2)
+
 ;; 33343
