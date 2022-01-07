41346ff0b4f21530dfd2b84347568ab7dad832be
Eric Tobin <eatobin@gmail.com>
2022-01-07 14:05:29 -0600

    Revert "day02.clj"
    This reverts commit 397a32fe32e6a688e6e828f930f43dd22084225a.

--- src/advent/day02.clj
+++ src/advent/day02.clj
@@ -1,8 +1,72 @@
 (ns advent.day02
-  (:require [advent.intcode :as ic]))
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
+
+; ABCDE
+;  1002
+
+; a- b- or c- = left-to-right position after 2 digit opcode
+; -p- -i- or -r- = position, immediate or relative mode
+; -r or -w = read or write
+
+(defn make-tv [file]
+  (->> (first (with-open [reader (io/reader file)]
+                (doall
+                  (csv/read-csv reader))))
+       (map #(Long/parseLong %))
+       (into [])
+       (zipmap (range))
+       (into (sorted-map-by <))))
+
+(def OFFSET-C 1)
+(def OFFSET-B 2)
+(def OFFSET-A 3)
+
+(defn char-to-int [char-as-byte]
+  (if (or (< char-as-byte 48)
+          (> char-as-byte 57))
+    "Char is not an integer"
+    (- char-as-byte 48)))
+
+(defn pad-5 [instruction]
+  (zipmap [:a :b :c :d :e]
+          (for [character (format "%05d" instruction)]
+            (char-to-int (byte character)))))
+
+(defn a-param [{:keys [instruction pointer memory]}]
+  (case (instruction :a)
+    ; a-p-w
+    0 (memory (+ pointer OFFSET-A))))
+
+(defn b-param [{:keys [instruction pointer memory]}]
+  (case (instruction :b)
+    ; b-p-r
+    0 (memory (memory (+ pointer OFFSET-B)))))
+
+(defn c-param [{:keys [instruction pointer memory]}]
+  (case (instruction :c)
+    ; c-p-r
+    0 (memory (memory (+ pointer OFFSET-C)))))
+
+(defn op-code [{:keys [pointer memory]}]
+  (let [instruction (pad-5 (memory pointer))]
+    (if (= 9 (instruction :d))
+      {:pointer pointer :memory memory}
+      (case (instruction :e)
+        1 (recur
+            {:pointer (+ 4 pointer)
+             :memory  (assoc memory (a-param {:instruction instruction :pointer pointer :memory memory})
+                                    (+ (c-param {:instruction instruction :pointer pointer :memory memory})
+                                       (b-param {:instruction instruction :pointer pointer :memory memory})))})
+        2 (recur
+            {:pointer (+ 4 pointer)
+             :memory  (assoc memory (a-param {:instruction instruction :pointer pointer :memory memory})
+                                    (* (c-param {:instruction instruction :pointer pointer :memory memory})
+                                       (b-param {:instruction instruction :pointer pointer :memory memory})))})
+        "Unknown opcode"))))
 
 ;part a
-(def tv (ic/make-tv "resources/day02.csv"))
+(def tv (make-tv "resources/day02.csv"))
 
 (defn updated-memory [noun verb]
   (->
@@ -11,8 +75,8 @@
     (assoc 2 verb)))
 
 (def answer
-  (((ic/op-code {:pointer 0
-                 :memory  (updated-memory 12 2)})
+  (((op-code {:pointer 0
+              :memory  (updated-memory 12 2)})
     :memory)
    0))
 
@@ -24,8 +88,8 @@
 (def noun-verb
   (vec (for [noun (range 0 100)
              verb (range 0 100)
-             :let [candidate (((ic/op-code {:pointer 0
-                                            :memory  (updated-memory noun verb)})
+             :let [candidate (((op-code {:pointer 0
+                                         :memory  (updated-memory noun verb)})
                                :memory)
                               0)]
              :when (= candidate 19690720)]
