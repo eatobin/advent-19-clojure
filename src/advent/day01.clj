91806ef22e039c9195f077ddfefa906218985464
Eric Tobin <eatobin@gmail.com>
2022-01-31 19:04:11 -0700

    day01 back

--- src/advent/day01.clj
+++ src/advent/day01.clj
@@ -5,43 +5,83 @@
             [orchestra.spec.test :as ostest]))
 
 ;part a
-(defn fuel [module]
-  (max
-    0
-    (- (quot module 3) 2)))
-(s/fdef fuel
+(defn gas [module]
+  (- (quot module 3) 2))
+(s/fdef gas
         :args (s/cat :module ::dom/module)
-        :ret ::dom/fuel)
+        :ret ::dom/gas)
 
 (def modules-sum (->>
                    "resources/day01.txt"
                    (slurp)
                    (str/split-lines)
-                   (map read-string)
-                   (map fuel)
+                   (map #(Integer/parseInt %))
+                   (into [])
+                   (map gas)
                    (reduce +)))
 
 (println modules-sum)
 
+(defn fuel [x]
+  (max 0
+       (- (Math/floor (/ x 3)) 2)))
+
+(def modules-sum-je (->>
+                      "resources/day01.txt"
+                      (slurp)
+                      (str/split-lines)
+                      (map #(Integer/parseInt %))
+                      (into [])
+                      (map fuel)
+                      (reduce +)
+                      (int)))
+
+(println modules-sum-je)
+
 ;; 3337766
 
 ;part b
-(defn fuel-plus [x]
-  (apply + (take-while pos? (next (iterate fuel x)))))
-(s/fdef fuel-plus
+(defn gas-plus [module]
+  (loop [m module
+         acc 0]
+    (let [new-gas (gas m)]
+      (if (pos? new-gas)
+        (recur
+          new-gas
+          (+ acc new-gas))
+        acc))))
+(s/fdef gas-plus
         :args (s/cat :module ::dom/module)
-        :ret ::dom/fuel)
+        :ret ::dom/gas)
 
-(def modules-fuel-sum (->>
-                        "resources/day01.txt"
-                        (slurp)
-                        (str/split-lines)
-                        (map read-string)
-                        (map fuel-plus)
-                        (reduce +)))
+(def modules-gas-sum (->>
+                       "resources/day01.txt"
+                       (slurp)
+                       (str/split-lines)
+                       (map #(Integer/parseInt %))
+                       (into [])
+                       (map gas-plus)
+                       (reduce +)))
 
-(println modules-fuel-sum)
+(println modules-gas-sum)
+
+(defn total-fuel [x]
+  (apply + (take-while pos? (next (iterate fuel x)))))
+
+(def modules-gas-sum-je (->>
+                          "resources/day01.txt"
+                          (slurp)
+                          (str/split-lines)
+                          (map #(Integer/parseInt %))
+                          (into [])
+                          (map total-fuel)
+                          (reduce +)
+                          (int)))
+
+(println modules-gas-sum-je)
 
 ;; 5003788
+
+(int (reduce + (map total-fuel (map read-string (clojure.string/split-lines (slurp "resources/day01.txt"))))))
 
 (ostest/instrument)
