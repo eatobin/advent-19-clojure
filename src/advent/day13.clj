dfd6815984d2688556626c01fedb962ae7caa123
Eric Tobin <eatobin@gmail.com>
2022-06-27 16:49:53 -0500

    start day13.clj

--- src/advent/day13.clj
+++ src/advent/day13.clj
@@ -1,1 +1,9 @@
-(ns advent.day13)
+(ns advent.day13
+  (:require [advent.intcode :as ic]))
+
+;part a
+(def tv (ic/make-tv "resources/day13.csv"))
+
+(def oc (atom {:input nil :output nil :phase nil :pointer 0 :relative-base 0 :memory tv :stopped? false :recur? false}))
+
+(swap! oc ic/op-code)
