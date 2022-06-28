97b87db5b16c0e55a6ba904dfac79e6fb46e9e49
Eric Tobin <eatobin@gmail.com>
2022-06-28 16:43:01 -0500

    conj output

--- src/advent/day13.clj
+++ src/advent/day13.clj
@@ -4,4 +4,4 @@
 ;part a
 (def tv (ic/make-tv "resources/day13.csv"))
 
-(ic/op-code {:input 666 :output [] :phase nil :pointer 0 :relative-base 0 :memory tv :stopped? false :recur? true})
+(ic/op-code {:input 0 :output [] :phase nil :pointer 0 :relative-base 0 :memory tv :stopped? false :recur? true})
