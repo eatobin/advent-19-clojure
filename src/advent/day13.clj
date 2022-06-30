d415bbe8a0bba1d910e06611f0b05598d1631a5e
Eric Tobin <eatobin@gmail.com>
2022-06-30 16:24:15 -0500

    part a works!!

--- src/advent/day13.clj
+++ src/advent/day13.clj
@@ -7,3 +7,9 @@
 (def raw-output ((ic/op-code {:input 0 :output [] :phase nil :pointer 0 :relative-base 0 :memory tv :stopped? false :recur? true}) :output))
 
 (def tiles (vec (map vec (partition 3 raw-output))))
+
+(defn is-block? [[_ _ id]] (if (= id 2) 1 0))
+
+(reduce + (map is-block? tiles))
+
+;; 412
