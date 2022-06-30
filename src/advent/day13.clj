2b29b3f494a09aa3c2c57533f551ee39d5342ec1
Eric Tobin <eatobin@gmail.com>
2022-06-30 16:33:15 -0500

    part a works!!- nil

--- src/advent/day13.clj
+++ src/advent/day13.clj
@@ -4,7 +4,7 @@
 ;part a
 (def tv (ic/make-tv "resources/day13.csv"))
 
-(def raw-output ((ic/op-code {:input 0 :output [] :phase nil :pointer 0 :relative-base 0 :memory tv :stopped? false :recur? true}) :output))
+(def raw-output ((ic/op-code {:input nil :output [] :phase nil :pointer 0 :relative-base 0 :memory tv :stopped? false :recur? true}) :output))
 
 (def tiles (vec (map vec (partition 3 raw-output))))
 
