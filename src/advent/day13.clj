76acd08ac91e20223f88e29038e480816015db1d
Eric Tobin <eatobin@gmail.com>
2022-06-30 18:08:23 -0500

    part b progress

--- src/advent/day13.clj
+++ src/advent/day13.clj
@@ -13,3 +13,18 @@
 (reduce + (map is-block? tiles))
 
 ;; 412
+
+;part b
+(def tv-b (assoc tv 0 2))
+
+(def raw-output-b-n ((ic/op-code {:input 0 :output [] :phase nil :pointer 0 :relative-base 0 :memory tv-b :stopped? false :recur? true}) :output))
+
+(def raw-output-b-l ((ic/op-code {:input -1 :output [] :phase nil :pointer 0 :relative-base 0 :memory tv-b :stopped? false :recur? true}) :output))
+
+(def raw-output-b-r ((ic/op-code {:input 1 :output [] :phase nil :pointer 0 :relative-base 0 :memory tv-b :stopped? false :recur? true}) :output))
+
+(def tiles-n (vec (map vec (partition 3 raw-output-b-n))))
+
+(def tiles-l (vec (map vec (partition 3 raw-output-b-l))))
+
+(def tiles-r (vec (map vec (partition 3 raw-output-b-r))))
