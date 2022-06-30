12ff91b8621750c4490e98539bd885fe19915330
Eric Tobin <eatobin@gmail.com>
2022-06-30 16:11:26 -0500

    progress

--- src/advent/day13.clj
+++ src/advent/day13.clj
@@ -4,4 +4,6 @@
 ;part a
 (def tv (ic/make-tv "resources/day13.csv"))
 
-(ic/op-code {:input 0 :output [] :phase nil :pointer 0 :relative-base 0 :memory tv :stopped? false :recur? true})
+(def raw-output ((ic/op-code {:input 0 :output [] :phase nil :pointer 0 :relative-base 0 :memory tv :stopped? false :recur? true}) :output))
+
+(def tiles (vec (map vec (partition 3 raw-output))))
