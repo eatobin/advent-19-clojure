1560005f9687e63a51c3fd30d602ddfd31c9ceba
Eric Tobin <eatobin@gmail.com>
2023-12-15 16:44:30 -0600

    comment

--- src/advent/day09.clj
+++ src/advent/day09.clj
@@ -2,18 +2,22 @@
   (:require [advent.intcode :as ic]))
 
 ;part a
-(def tv (ic/make-tv "resources/day09.csv"))
+(def memory (ic/make-tv "resources/day09.csv"))
 
 
-(def answer (last ((ic/op-code {:input 1 :output [] :phase nil :pointer 0 :relative-base 0 :memory tv :stopped? false :recur? true}) :output)))
+(def answer (last ((ic/op-code {:input 1 :output [] :phase nil :pointer 0 :relative-base 0 :memory memory :stopped? false :recur? true}) :output)))
 
-(println answer)
+(comment
+  answer
+  )
 
 ; 3780860499
 
 ; part b
-(def answer-2 (last ((ic/op-code {:input 2 :output [] :phase nil :pointer 0 :relative-base 0 :memory tv :stopped? false :recur? true}) :output)))
+(def answer-2 (last ((ic/op-code {:input 2 :output [] :phase nil :pointer 0 :relative-base 0 :memory memory :stopped? false :recur? true}) :output)))
 
-(println answer-2)
+(comment
+  answer-2
+  )
 
 ; 33343
