adcb5e4ae829fb0250ef93c7b43f7094a9058276
Eric Tobin <eatobin@gmail.com>
2025-06-25 20:25:24 -0500

    progress

--- day02/src/day02/day02.clj
+++ day02/src/day02/day02.clj
@@ -1,6 +1,32 @@
 (ns day02.day02)
 
-; clj -M:repl/nREPL
+;; clj -M:repl/nREPL
+
+;; Instruction:
+;; ABCDE
+;; 01234
+;; 01002
+;; 34(DE) - two-digit opcode,      02 == opcode 2
+;;  2(C) - mode of 1st parameter,  0 == position mode
+;;  1(B) - mode of 2nd parameter,  1 == immediate mode
+;;  0(A) - mode of 3rd parameter,  0 == position mode,
+;;                                   omitted due to being a leading zero
+;; 0 1 or 2 = left-to-right position after 2 digit opcode
+;; p i or r = position, immediate or relative mode
+;; r or w = read or write
+
+(def memory [1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 10, 1, 19, 2, 9, 19, 23, 2, 13, 23, 27, 1, 6, 27, 31, 2, 6, 31, 35, 2, 13, 35, 39, 1, 39, 10, 43, 2, 43, 13, 47, 1, 9, 47, 51, 1, 51, 13, 55, 1, 55, 13, 59, 2, 59, 13, 63, 1, 63, 6, 67, 2, 6, 67, 71, 1, 5, 71, 75, 2, 6, 75, 79, 1, 5, 79, 83, 2, 83, 6, 87, 1, 5, 87, 91, 1, 6, 91, 95, 2, 95, 6, 99, 1, 5, 99, 103, 1, 6, 103, 107, 1, 107, 2, 111, 1, 111, 5, 0, 99, 2, 14, 0, 0])
+
+(defn char-to-int [char-as-byte]
+  (if (or (< char-as-byte 48)
+          (> char-as-byte 57))
+    "Char is not an integer"
+    (- char-as-byte 48)))
+
+(defn pad-5 [instruction]
+  (zipmap [:a :b :c :d :e]
+          (for [character (format "%05d" instruction)]
+            (char-to-int (byte character)))))
 
 (def OFFSET-C 1)
 (def OFFSET-B 2)
@@ -31,8 +57,6 @@
         (multiply {:pointer pointer, :memory memory}))
     99 {:pointer pointer
         :memory  memory}))
-
-(def memory [1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 10, 1, 19, 2, 9, 19, 23, 2, 13, 23, 27, 1, 6, 27, 31, 2, 6, 31, 35, 2, 13, 35, 39, 1, 39, 10, 43, 2, 43, 13, 47, 1, 9, 47, 51, 1, 51, 13, 55, 1, 55, 13, 59, 2, 59, 13, 63, 1, 63, 6, 67, 2, 6, 67, 71, 1, 5, 71, 75, 2, 6, 75, 79, 1, 5, 79, 83, 2, 83, 6, 87, 1, 5, 87, 91, 1, 6, 91, 95, 2, 95, 6, 99, 1, 5, 99, 103, 1, 6, 103, 107, 1, 107, 2, 111, 1, 111, 5, 0, 99, 2, 14, 0, 0])
 
 (defn updated-memory [noun verb]
   (->
