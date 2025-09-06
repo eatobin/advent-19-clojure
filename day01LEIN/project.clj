ca074725fe801c094222523d066e69d1b4be8545
Eric Tobin <eatobin@gmail.com>
2025-09-05 22:00:24 -0700

    bump clojure

--- day01LEIN/project.clj
+++ day01LEIN/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.1"]
+  :dependencies [[org.clojure/clojure "1.12.2"]
                  [metosin/malli "0.19.1"]
                  [org.babashka/sci "0.10.47"]]
   :main ^:skip-aot day01lein.day01lein
