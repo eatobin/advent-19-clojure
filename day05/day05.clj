9300ed071a64e227356944ba2b6b8ae6f686a5cc
Eric Tobin <eatobin@gmail.com>
2025-09-14 20:47:17 -0700

    start day05

---
+++ day05/day05.clj
@@ -0,0 +1,22 @@
+(ns day05.day05
+  (:require [day05.intcode :as ic]))
+
+;part a
+(def memory (ic/make-memory "day05.csv"))
+
+(def answer (last ((ic/op-code {:input 1 :output [] :phase nil :pointer 0 :relative-base 0 :memory memory :stopped? false :recur? true}) :output)))
+
+(comment
+  answer
+  )
+
+;9025675
+
+;part b
+(def answer-2 (last ((ic/op-code {:input 5 :output [] :phase nil :pointer 0 :relative-base 0 :memory memory :stopped? false :recur? true}) :output)))
+
+(comment
+  answer-2
+  )
+
+;11981754
