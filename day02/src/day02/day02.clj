eb0fb239b280c0993a936bf55ecb81501b6a8da4
Eric Tobin <eatobin@gmail.com>
2026-02-22 16:40:44 -0700

    progress

--- day02/src/day02/day02.clj
+++ day02/src/day02/day02.clj
@@ -25,41 +25,37 @@
 
 (def memory [1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 10, 1, 19, 2, 9, 19, 23, 2, 13, 23, 27, 1, 6, 27, 31, 2, 6, 31, 35, 2, 13, 35, 39, 1, 39, 10, 43, 2, 43, 13, 47, 1, 9, 47, 51, 1, 51, 13, 55, 1, 55, 13, 59, 2, 59, 13, 63, 1, 63, 6, 67, 2, 6, 67, 71, 1, 5, 71, 75, 2, 6, 75, 79, 1, 5, 79, 83, 2, 83, 6, 87, 1, 5, 87, 91, 1, 6, 91, 95, 2, 95, 6, 99, 1, 5, 99, 103, 1, 6, 103, 107, 1, 107, 2, 111, 1, 111, 5, 0, 99, 2, 14, 0, 0])
 
-(defn char-to-int [char-as-byte]
-  (if (or (< char-as-byte 48)
-          (> char-as-byte 57))
-    "Char is not an integer"
-    (- char-as-byte 48)))
-
 (defn pad-5 [instruction]
   (zipmap [:a :b :c :d :e]
           (for [character (format "%05d" instruction)]
-            (char-to-int (byte character)))))
+            (- (byte character) 48))))
 
-(def OFFSET-C 1)
-(def OFFSET-B 2)
-(def OFFSET-A 3)
+(def POINTER-OFFSET-C 1)
+(def POINTER-OFFSET-B 2)
+(def POINTER-OFFSET-A 3)
 ;;change to haskell
-(defn write-to-read-from-index [{:keys [pointer memory]} offset]
-  (get memory (+ pointer offset)))
+(defn key-to-key [{:keys [pointer memory]} pointer-offset]
+  (get memory (+ pointer pointer-offset)))
 
-(defn -p-w [{:keys [pointer memory]} offset]
-  (write-to-read-from-index {:pointer pointer :memory memory} offset))
+(defn -p-w [{:keys [pointer memory]} pointer-offset]
+  (key-to-key {:pointer pointer :memory memory} pointer-offset))
 
-(defn -p-r [{:keys [pointer memory]} offset]
-  (get memory (write-to-read-from-index {:pointer pointer :memory memory} offset)))
-;;change to generec with offset
+(defn -p-r [{:keys [pointer memory]} pointer-offset]
+  (get memory (key-to-key {:pointer pointer :memory memory} pointer-offset)))
+
+
+;;change to generic with offset
 (defn a-param [{:keys [instruction pointer memory]}]
   (case (instruction :a)
-    0 (-p-w {:pointer pointer :memory memory} OFFSET-A)))   ; a-p-w
+    0 (-p-w {:pointer pointer :memory memory} POINTER-OFFSET-A))) ; a-p-w
 
 (defn b-param [{:keys [instruction pointer memory]}]
   (case (instruction :b)
-    0 (-p-r {:pointer pointer :memory memory} OFFSET-B)))   ; b-p-r
+    0 (-p-r {:pointer pointer :memory memory} POINTER-OFFSET-B))) ; b-p-r
 
 (defn c-param [{:keys [instruction pointer memory]}]
   (case (instruction :c)
-    0 (-p-r {:pointer pointer :memory memory} OFFSET-C)))   ; c-p-r
+    0 (-p-r {:pointer pointer :memory memory} POINTER-OFFSET-C))) ; c-p-r
 
 ;;part a
 (defn add [{:keys [instruction pointer memory]}]
