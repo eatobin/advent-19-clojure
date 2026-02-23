6dc3ba4b857d825255a35c0d2142c17cc2363f3c
Eric Tobin <eatobin@gmail.com>
2026-02-23 16:56:53 -0700

    use app template

---
+++ day02/test/day02/day02_test.clj
@@ -0,0 +1,7 @@
+(ns day02.day02-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [day02.day02 :as sut])) ; system under test
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 0))))
