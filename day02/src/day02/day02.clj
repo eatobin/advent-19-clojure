a6c2f308308386a729a69e8de6a60861f4e9db7a
Eric Tobin <eatobin@gmail.com>
2026-02-23 19:32:53 -0700

    in process

--- day02/src/day02/day02.clj
+++ day02/src/day02/day02.clj
@@ -30,7 +30,7 @@
        (map parse-long)
        (apply list)))
 
-;(def memory-as-csv-string "1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 10, 1, 19, 2, 9, 19, 23, 2, 13, 23, 27, 1, 6, 27, 31, 2, 6, 31, 35, 2, 13, 35, 39, 1, 39, 10, 43, 2, 43, 13, 47, 1, 9, 47, 51, 1, 51, 13, 55, 1, 55, 13, 59, 2, 59, 13, 63, 1, 63, 6, 67, 2, 6, 67, 71, 1, 5, 71, 75, 2, 6, 75, 79, 1, 5, 79, 83, 2, 83, 6, 87, 1, 5, 87, 91, 1, 6, 91, 95, 2, 95, 6, 99, 1, 5, 99, 103, 1, 6, 103, 107, 1, 107, 2, 111, 1, 111, 5, 0, 99, 2, 14, 0, 0")
+;(def memory-as-csv-string "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,2,9,19,23,2,13,23,27,1,6,27,31,2,6,31,35,2,13,35,39,1,39,10,43,2,43,13,47,1,9,47,51,1,51,13,55,1,55,13,59,2,59,13,63,1,63,6,67,2,6,67,71,1,5,71,75,2,6,75,79,1,5,79,83,2,83,6,87,1,5,87,91,1,6,91,95,2,95,6,99,1,5,99,103,1,6,103,107,1,107,2,111,1,111,5,0,99,2,14,0,0")
 
 (defn make-intcode [pointer memory-as-csv-string]
   {:pointer pointer :memory (zipmap (range) (csv-to-ints memory-as-csv-string))})
@@ -52,7 +52,6 @@
 
 (defn -p-r [{:keys [pointer memory]} pointer-offset]
   (get memory (key-to-key {:pointer pointer :memory memory} pointer-offset)))
-
 
 ;;change to generic with offset
 (defn a-param [{:keys [instruction pointer memory]}]
