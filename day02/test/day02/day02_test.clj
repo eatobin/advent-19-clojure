39128ed775aeb0e97f302847d8de11893f410408
Eric Tobin <eatobin@gmail.com>
2026-02-25 11:59:57 -0700

    testing progress

--- day02/test/day02/day02_test.clj
+++ day02/test/day02/day02_test.clj
@@ -9,3 +9,11 @@
 (deftest make-intcode
   (testing "make-intcode makes an intcode"
     (is (= (sut/make-intcode 0 "10,11,12") {:pointer 0 :memory {0 10 1 11 2 12}}))))
+
+(deftest make-instruction-1
+  (testing "make-instruction makes a 1 digit instruction"
+    (is (= (sut/make-instruction 1) {:a 0, :b 0, :c 0, :d 0, :e 1}))))
+
+(deftest make-instruction-5
+  (testing "make-instruction makes a 5 digit instruction"
+    (is (= (sut/make-instruction 12345) {:a 1, :b 2, :c 3, :d 4, :e 5}))))
