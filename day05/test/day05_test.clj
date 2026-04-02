641951202817740496eb2cf5d49278bc4e9f3c61
Eric Tobin <eatobin@gmail.com>
2026-04-01 22:35:40 -0700

    start haskell layout

---
+++ day05/test/day05_test.clj
@@ -0,0 +1,7 @@
+(ns day05-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [main :as sut])) ; system under test
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
