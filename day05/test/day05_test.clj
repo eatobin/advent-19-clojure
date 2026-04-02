836f2bf41697f423b93cd3836211b686a76fcd38
Eric Tobin <eatobin@gmail.com>
2026-04-01 22:42:15 -0700

    format

--- day05/test/day05_test.clj
+++ day05/test/day05_test.clj
@@ -1,6 +1,6 @@
 (ns day05-test
   (:require [clojure.test :refer [deftest is testing]]
-            [main :as sut])) ; system under test
+            [main :as sut]))                                ; system under test
 
 (deftest a-test
   (testing "FIXME, I fail."
