abc7f5928747ef39c8e0d9dc56a9796aabe0d9aa
Eric Tobin <eatobin@gmail.com>
2026-04-05 20:56:31 -0700

    bye to char

--- day05/src/eatobin/library.clj
+++ day05/src/eatobin/library.clj
@@ -2,13 +2,10 @@
   (:require
    [clojure.string :as str]))
 
-(defn char-to-int [char-as-byte]
-  (- char-as-byte 48))
-
 (defn make-instruction [integer]
   (zipmap [:a :b :c :d :e]
           (for [character (format "%05d" integer)]
-            (char-to-int (byte character)))))
+            (- (byte character) 48))))
 
 (defn make-memory [memory-as-csv-string]
   (->>
