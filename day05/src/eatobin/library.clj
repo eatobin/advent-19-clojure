b278a745a202d713f71dc6e9158af870809e3665
Eric Tobin <eatobin@gmail.com>
2026-04-05 20:52:04 -0700

    make memory tests

--- day05/src/eatobin/library.clj
+++ day05/src/eatobin/library.clj
@@ -1,4 +1,6 @@
-(ns eatobin.library)
+(ns eatobin.library
+  (:require
+   [clojure.string :as str]))
 
 (defn char-to-int [char-as-byte]
   (- char-as-byte 48))
@@ -7,5 +9,14 @@
   (zipmap [:a :b :c :d :e]
           (for [character (format "%05d" integer)]
             (char-to-int (byte character)))))
+
+(defn make-memory [memory-as-csv-string]
+  (->>
+   (str/split memory-as-csv-string #",")
+   (map #(Long/parseLong %))
+   (into [])
+   (zipmap (range))
+   (into (sorted-map))))
+
 (comment
-  (make-instruction 6))
+  (make-memory "10,11,1"))
--- day05/test/eatobin/library_test.clj
+++ day05/test/eatobin/library_test.clj
@@ -7,6 +7,9 @@
 (def instruction3 {:a 0, :b 0, :c 4, :d 5, :e 6})
 (def instruction5 {:a 2, :b 3, :c 4, :d 5, :e 6})
 
+(def memory-as-csv-string "10,11,1")
+(def this-memory {0 10, 1 11, 2 1})
+
 (deftest a-test-alone
   (testing "a test all by itself"
     (is (= 78 78))))
@@ -22,3 +25,7 @@
 (deftest make-instruction-5
   (testing "make an Instruction 5"
     (is (= (sut/make-instruction 23456) instruction5))))
+
+(deftest make-memory
+  (testing "make a memory"
+    (is (= (sut/make-memory memory-as-csv-string) this-memory))))
