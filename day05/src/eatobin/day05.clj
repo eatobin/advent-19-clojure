c1fb1a51b2e7ac3f4c4a7658265b46f1c67ee0ed
Eric Tobin <eatobin@gmail.com>
2026-04-06 17:04:15 -0700

    bye exe

--- day05/src/eatobin/day05.clj
+++ day05/src/eatobin/day05.clj
@@ -1,10 +1,10 @@
 (ns eatobin.day05
   "FIXME: my new org.corfield.new/scratch project.")
 
-(defn exec
-  "Invoke me with clojure -X eatobin.day05/exec"
-  [opts]
-  (println "exec with" opts))
+;; (defn exec
+;;   "Invoke me with clojure -X eatobin.day05/exec"
+;;   [opts]
+;;   (println "exec with" opts))
 
 (defn -main
   "Invoke me with clojure -M -m eatobin.day05"
