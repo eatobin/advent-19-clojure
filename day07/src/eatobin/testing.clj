1b02a220345bdb3432adca44c2dbce1c15e81dfd
Eric Tobin <eatobin@gmail.com>
2026-05-17 12:11:30 -0700

    three

--- day07/src/eatobin/testing.clj
+++ day07/src/eatobin/testing.clj
@@ -1,37 +1,40 @@
 (ns eatobin.testing)
 
-(def user-map-1 {:message "one" :count 0})
-(def user-map-2 {:message "two" :count 0})
-(def pair {1 user-map-1 2 user-map-2})
+(def user-map-1 {:input 1 :output 0})
+(def user-map-2 {:input 0 :output 0})
+(def user-map-3 {:input 0 :output 0})
+(def triple {1 user-map-1 2 user-map-2 3 user-map-3})
 
-(let [user {:first "Jane" :last "Doe" :age 30}]
-  (let [{:keys [first last]} user]
-    (str first " " last)))
+;; (let [user {:first "Jane" :last "Doe" :age 30}]
+;;   (let [{:keys [first last]} user]
+;;     (str first " " last)))
+;; 
+;; (defn make-user [user-map]
+;;   (let [{:keys [first last]} user-map]
+;;     (str first " " last)))
+;; 
+;; (defn ccc [{:keys [first last]}]
+;;   (str first " " last))
 
-(defn make-user [user-map]
-  (let [{:keys [first last]} user-map]
-    (str first " " last)))
+(defn increment-output [user-map]
+  (update user-map :output inc))
 
-(defn ccc [{:keys [first last]}]
-  (str first " " last))
-
-(defn increment-count [user-map]
-  (update user-map :count inc))
-
-(println (increment-count user-map-1))
+(increment-output user-map-1)
 
 (defn till-3 [user-map]
-  (if (= (:count user-map) 3)
+  (if (= (:output user-map) 3)
     user-map
     (recur
-     (increment-count user-map))))
+     (increment-output user-map))))
 
 (till-3 user-map-1)
 
-(defn both [index pair]
-  (if (= (get-in pair [index :count]) 3)
-    pair
-    (recur
-     index (update-in pair [index :count] inc))))
+(defn three [index triple]
+  (loop [index index
+         triple triple]
+    (if (= (get-in triple [index :output]) 3)
+      triple
+      (recur
+       index (update-in triple [index :output] inc)))))
 
-(both 1 pair)
+(three 1 triple)
