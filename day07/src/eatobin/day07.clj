8f9ce0bd46a87b5cedf1d8e38bc9d6634a552c57
Eric Tobin <eatobin@gmail.com>
2026-05-25 18:55:03 -0700

    b broken

--- day07/src/eatobin/day07.clj
+++ day07/src/eatobin/day07.clj
@@ -67,3 +67,55 @@
   368584
   :rcf)
 
+;part b
+(def possibles-2 (into ()
+                       (reverse
+                        (for [a (range 5 10)
+                              b (range 5 10)
+                              c (range 5 10)
+                              d (range 5 10)
+                              e (range 5 10)
+                              :when (distinct? a b c d e)]
+                          [a b c d e]))))
+
+(def a-pass-map
+  {1 {:input 0 :output [] :phase (test-possibility 0) :pointer 0 :relative-base 0 :memory memory :stopped? false :recur? true}
+   2 {:input 0 :output [] :phase (test-possibility 1) :pointer 0 :relative-base 0 :memory memory :stopped? false :recur? true}
+   3 {:input 0 :output [] :phase (test-possibility 2) :pointer 0 :relative-base 0 :memory memory :stopped? false :recur? true}
+   4 {:input 0 :output [] :phase (test-possibility 3) :pointer 0 :relative-base 0 :memory memory :stopped? false :recur? true}
+   5 {:input 0 :output [] :phase (test-possibility 4) :pointer 0 :relative-base 0 :memory memory :stopped? false :recur? true}})
+
+(defn grab-my-input-from-last-output [index this-pass-map]
+  (if (= index 1)
+    (assoc-in this-pass-map [index :input] (last (get-in this-pass-map [5 :output])))
+    (assoc-in this-pass-map [index :input] (last (get-in this-pass-map [(dec index) :output])))))
+
+(defn grab-and-run-2 [index this-pass-map]
+  (->>
+   this-pass-map
+   (grab-my-input-from-last-output index)
+   (run-my-output-from-my-input index)))
+
+
+(defn pass-2 [[a b c d e]]
+  (loop [index         1
+         this-pass-map {1 {:input 0 :output [] :phase a :pointer 0 :relative-base 0 :memory memory :stopped? false :recur? false}
+                        2 {:input 0 :output [] :phase b :pointer 0 :relative-base 0 :memory memory :stopped? false :recur? false}
+                        3 {:input 0 :output [] :phase c :pointer 0 :relative-base 0 :memory memory :stopped? false :recur? false}
+                        4 {:input 0 :output [] :phase d :pointer 0 :relative-base 0 :memory memory :stopped? false :recur? false}
+                        5 {:input 0 :output [] :phase e :pointer 0 :relative-base 0 :memory memory :stopped? false :recur? false}}]
+    (if (get-in this-pass-map [5 :stopped?])
+      (first (get-in this-pass-map [5 :output]))
+      (recur
+       (inc (mod index 5))
+       (grab-and-run-2 index this-pass-map)))))
+
+(defn passes-2 []
+  (map #(pass-2 %) possibles-2))
+
+(def answer-2 (apply max (passes-2)))
+
+(comment
+  answer-2
+  35993240
+  :rcf)
