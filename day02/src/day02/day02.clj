bd740619d4293814483fb31309b54265e4a44318
Eric Tobin <eatobin@gmail.com>
2026-08-02 11:46:45 -0700

    works!!!

--- day02/src/day02/day02.clj
+++ day02/src/day02/day02.clj
@@ -35,8 +35,8 @@
 
   (let [memory-as-csv-string "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,2,9,19,23,2,13,23,27,1,6,27,31,2,6,31,35,2,13,35,39,1,39,10,43,2,43,13,47,1,9,47,51,1,51,13,55,1,55,13,59,2,59,13,63,1,63,6,67,2,6,67,71,1,5,71,75,2,6,75,79,1,5,79,83,2,83,6,87,1,5,87,91,1,6,91,95,2,95,6,99,1,5,99,103,1,6,103,107,1,107,2,111,1,111,5,0,99,2,14,0,0"
         initial-memory       (lib/make-memory memory-as-csv-string)
-        initial-state        {:pointer 0 :memory (lib/updated-memory 12 2 initial-memory) :actions '()}
-        final-state-a        (lib/run-op-code initial-state)
+        initial-state-a      {:pointer 0 :memory (lib/updated-memory 12 2 initial-memory) :actions '()}
+        final-state-a        (lib/run-op-code initial-state-a)
         answer-1             (get (:memory final-state-a) 0)]
 
     ;; part a
@@ -47,15 +47,10 @@
     (let [noun    (first (first (first (find-winner))))
           verb    (last (first (first (find-winner))))
           answer2 (+ (* 100 noun) verb)]
-      (printf "%nPart B answer: %s, correct: 8226%n" answer2))))
-;  (let [final-state-b (first (for [noun (range 0 100)
-;                                   verb (range 0 100)
-;                                   :let [candidate-intcode (lib/run-op-code {:pointer 0 :memory (lib/updated-memory noun verb initial-memory) :actions '()})
-;                                         candidate         ((:memory candidate-intcode) 0)]
-;                                   :when (= candidate 19690720)]
-;                               [candidate-intcode (+ (* 100 noun) verb)]))]
-;    (printf "%nPart B answer: %s, correct: 8226%n" (last final-state-b))
-;    (println (reverse (:actions (first final-state-b)))))))
+      (printf "%nPart B answer: %s, correct: 8226%n" answer2)
+      (let [initial-state-b {:pointer 0 :memory (lib/updated-memory noun verb initial-memory) :actions '()}
+            final-state-b   (lib/run-op-code initial-state-b)]
+        (println (reverse (:actions final-state-b)))))))
 
 (comment
   (-main)
