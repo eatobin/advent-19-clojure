f165790f5b746c70e399cf8a4bc9d8c0e5f516ff
Eric Tobin <eatobin@gmail.com>
2026-08-19 12:28:11 -0700

    back to desktop

--- day02/test/day02/day02_test.clj
+++ day02/test/day02/day02_test.clj
@@ -1,133 +1,35 @@
 (ns day02.day02-test
   (:require [clojure.test :refer [deftest is testing]]
-            [matcher-combinators.test]
-            [matcher-combinators.matchers :as m]
-            [day02.library :as sut])
-  (:import (clojure.lang ExceptionInfo))) ; system under test
+            [day02.library :as sut]))                       ; system under test
 
-(def instruction-1 {:a 0, :b 0, :c 0, :d 0, :e 6})
-(def instruction-3 {:a 0, :b 0, :c 4, :d 5, :e 6})
-(def instruction-5 {:a 2, :b 3, :c 4, :d 5, :e 6})
 (def memory-as-csv-string "10,11,1")
-(def this-memory [10 11 1])
-(def intcode {:pointer 0
-              :memory  this-memory
-              :actions '()})
-(def this-memory-x [0 3 2 33])
-(def intcode-x {:pointer 0
-                :memory  this-memory-x
-                :actions '()})
-(def this-memory-add-mult [0 2 1 0])
-(def aoc-memory-1 "1,0,0,3,99")
-(def aoc-memory-2 "1,9,10,3,2,3,11,0,99,30,40,50")
-(def aoc-memory-3 "1,0,0,0,99")
-;; (def aoc-memory-4 "2,3,0,3,99")
-;; (def aoc-memory-5 "2,4,4,5,99,0")
-;; (def aoc-memory-6 "1,1,1,4,99,5,6,0,99")
-(def intcode-add-mult-exit {:pointer 0
-                            :memory  this-memory-add-mult
-                            :actions '()})
-(def intcode-add {:pointer 4
-                  :memory  [3 2 1 0]
-                  :actions '(:add)})
-(def intcode-mult {:pointer 4
-                   :memory  [2 2 1 0]
-                   :actions '(:multiply)})
-(def intcode-exit {:pointer 0
-                   :memory  this-memory-add-mult
-                   :actions '(:exit)})
+(def intCode {:pointer 0 :memory {0 10 1 11 2 1}})
 
-(deftest a-stand-alone-test
+(deftest a-test
   (testing "This is a stand-alone test."
     (is (=
          1
          1))))
 
-(deftest test-matching-with-explicit-matchers
-  (is (match? (m/equals 37) (+ 29 8)))
-  (is (match? (m/regex #"fox") "The quick brown fox jumps over the lazy dog")))
-
-(deftest make-instructions
-  (testing "make various instructions"
-    (testing "make-instruction makes a 1 digit instruction"
-      (is (=
-           instruction-1
-           (sut/make-instruction 6))))
-    (testing "make-instruction makes a 3 digit instruction"
-      (is (=
-           instruction-3
-           (sut/make-instruction 456))))
-    (testing "make-instruction makes a 5 digit instruction"
-      (is (=
-           instruction-5
-           (sut/make-instruction 23456))))))
-
-(deftest make-memory
-  (testing "make a Memory"
+(deftest make-instruction-1
+  (testing "make-instruction makes a 1 digit instruction"
     (is (=
-         this-memory
-         (sut/make-memory memory-as-csv-string)))))
+         {:a 0, :b 0, :c 0, :d 0, :e 1}
+         (sut/make-instruction 1)))))
 
-(deftest memory-lookup
-  (testing "lookup various memory locations"
-    (testing "lookup a valid Memory index - -p-w"
-      (is (= 1
-             (sut/-p-w intcode 2))))
-    (testing "lookup a valid Memory index - -p-r"
-      (is (= 11
-             (sut/-p-r intcode 2))))
-    (testing "lookup an invalid Memory index - -p-w"
-      (is (nil?
-           (sut/-p-w intcode 33))))))
+(deftest make-instruction-5
+  (testing "make-instruction makes a 5 digit instruction"
+    (is (=
+         {:a 1, :b 2, :c 3, :d 4, :e 5}
+         (sut/make-instruction 12345)))))
 
-(deftest params-lookup
-  (testing "lookup various params"
-    (testing "lookup a valid aParam"
-      (is (= 33
-             (sut/a-param instruction-1 intcode-x))))
-    (testing "lookup a valid bParam"
-      (is (= 2
-             (sut/b-param instruction-1 intcode-x))))
-    (testing "lookup a valid cParam"
-      (is (= 33
-             (sut/c-param instruction-1 intcode-x))))
-    (testing "lookup a valid cParam with a bad instruction"
-      (is (thrown-match? ExceptionInfo
-                         {:error-type :bad-c-param-choice}
-                         (sut/c-param instruction-5 intcode-x))))))
+(deftest -p-w
+  (testing "-p-w"
+    (is (= 1
+           (sut/-p-w intCode 2)))))
 
-(deftest opcode-actions
-  (testing "add and multiply"
-    (testing "1 plus 2 should be set at 0 and pointer should be 4"
-      (is (= intcode-add
-             (sut/add instruction-1 intcode-add-mult-exit))))
-    (testing "1 times 2 should be set at 0 and pointer should be 4"
-      (is (= intcode-mult
-             (sut/multiply instruction-1 intcode-add-mult-exit))))
-    (testing "exit should just add an Exit to the actions and return the intCode"
-      (is (= intcode-exit
-             (sut/exit intcode-add-mult-exit))))))
-
-(deftest aoc-memory-tests
-  (testing "aoc-memory-tests"
-    (testing "aoc-memory-test-1"
-      (is (= {:pointer 4
-              :memory  [1 0 0 2 99]
-              :actions '(:exit :add)}
-             (sut/run-op-code {:pointer 0
-                               :memory  (sut/make-memory aoc-memory-1)
-                               :actions '()}))))
-    (testing "aoc-memory-test-2"
-      (is (= {:pointer 8
-              :memory  [3500 9 10 70 2 3 11 0 99 30 40 50]
-              :actions '(:exit :multiply :add)}
-             (sut/run-op-code {:pointer 0
-                               :memory  (sut/make-memory aoc-memory-2)
-                               :actions '()}))))
-    (testing "aoc-memory-test-3"
-      (is (= {:pointer 4
-              :memory  [2 0 0 0 99]
-              :actions '(:exit :add)}
-             (sut/run-op-code {:pointer 0
-                               :memory  (sut/make-memory aoc-memory-3)
-                               :actions '()}))))))
+(deftest -p-r
+  (testing "-p-r"
+    (is (=
+         11
+         (sut/-p-r intCode 2)))))
