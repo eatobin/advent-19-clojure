9a1c41993c0309a56e0e3e05d2375fefc769fc75
Eric Tobin <eatobin@gmail.com>
2026-08-30 12:48:55 -0700

    sample works

--- day03/src/day03/day03.clj
+++ day03/src/day03/day03.clj
@@ -79,9 +79,14 @@
 (def red-v-i (map-indexed vector red-v))
 (def red-blue-intersect (clojure.set/intersection red-set blue-set))
 
+;R75,D30,R83,U83,L12,D49,R71,U7,L72
+;U62,R66,U55,R34,D71,R55,D58,R83 = 610 steps
+;R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+;U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = 410 steps
+
 (comment
-  (def red-v (make-path "R5" [0 0]))
-  (def blue-v (make-path "R5" [3 0]))
+  (def red-v (make-paths ["R75" "D30" "R83" "U83" "L12" "D49" "R71" "U7" "L72"] [0 0]))
+  (def blue-v (make-paths ["U62" "R66" "U55" "R34" "D71" "R55" "D58" "R83"] [0 0]))
   (def red-set (set red-v))
   (def blue-set (set blue-v))
   (def red-blue-intersect (clojure.set/intersection red-set blue-set))
@@ -100,8 +105,13 @@
             [k v])))
   reds-k-v
   blues-k-v
+  (def answer-2 (apply min (for [[kr vr] reds-k-v
+                                 [kb vb] blues-k-v
+                                 :when (= vr vb)]
+                             (+ kr kb))))
+  answer-2
+  610
   :rcf)
-
 
 (def reds-k-v
   (rest (for [[k v] red-v-i
